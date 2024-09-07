64c471d0ec90800aee738a2088c2d27bd725c451
WenXin2 <dwnewville@gmail.com>
2024-09-07 13:17:20 -0500

    Fix item count

--- src/main/java/com/wenxin2/marioverse/blocks/QuestionBlock.java
+++ src/main/java/com/wenxin2/marioverse/blocks/QuestionBlock.java
@@ -42,6 +42,11 @@ public class QuestionBlock extends Block implements EntityBlock {
         this.registerDefaultState(this.stateDefinition.any().setValue(EMPTY, Boolean.TRUE));
     }
 
+    @Override
+    protected void createBlockStateDefinition(StateDefinition.Builder<Block, BlockState> stateBuilder) {
+        stateBuilder.add(EMPTY);
+    }
+
     @Nullable
     @Override
     public BlockEntity newBlockEntity(BlockPos pos, BlockState state) {
@@ -49,8 +54,6 @@ public class QuestionBlock extends Block implements EntityBlock {
     }
 
     @Override
-    protected void createBlockStateDefinition(StateDefinition.Builder<Block, BlockState> stateBuilder) {
-        stateBuilder.add(EMPTY);
     }
 
     @Override
@@ -116,29 +119,29 @@ public class QuestionBlock extends Block implements EntityBlock {
         if (stack.getItem() instanceof SpawnEggItem spawnEgg && ConfigRegistry.QUESTION_SPAWNS_MOBS.get()) {
             EntityType<?> entityType = spawnEgg.getType(stack);
 
-            if (world instanceof ServerLevel serverWorld && !entityType.is(TagRegistry.QUESTION_BLOCK_BLACKLIST)) { // Check this
+            if (world instanceof ServerLevel serverWorld && !entityType.is(TagRegistry.QUESTION_BLOCK_BLACKLIST)) {
                 if (world.getBlockState(pos.above()).isAir())
                     entityType.spawn(serverWorld, stack, null, pos.above(2), MobSpawnType.SPAWN_EGG, true, true);
                 else entityType.spawn(serverWorld, stack, null, pos.below((int) entityType.getHeight()), MobSpawnType.SPAWN_EGG, true, true);
-                stack.split(1);
+                stack.copyWithCount(1);
             } else if (world.getBlockState(pos.above()).isAir()) {
-                ItemEntity itemEntity = new ItemEntity(world, pos.getX() + 0.5D, pos.getY() + 1.0D, pos.getZ() + 0.5D, stack.split(1));
+                ItemEntity itemEntity = new ItemEntity(world, pos.getX() + 0.5D, pos.getY() + 1.0D, pos.getZ() + 0.5D, stack.copyWithCount(1));
                 world.addFreshEntity(itemEntity);
             } else {
-                ItemEntity itemEntity = new ItemEntity(world, pos.getX() + 0.5D, pos.getY() - 0.5D, pos.getZ() + 0.5D, stack.split(1));
+                ItemEntity itemEntity = new ItemEntity(world, pos.getX() + 0.5D, pos.getY() - 0.5D, pos.getZ() + 0.5D, stack.copyWithCount(1));
                 world.addFreshEntity(itemEntity);
             }
         } else if (stack.getItem() == Items.ARMOR_STAND) {
             ArmorStand armorStand = new ArmorStand(EntityType.ARMOR_STAND, world);
             armorStand.setPos(pos.getX() + 0.5D, pos.getY() + 1.0D, pos.getZ() + 0.5D);
             world.addFreshEntity(armorStand);
-            stack.split(1);
+            stack.copyWithCount(1);
         } else {
             if (world.getBlockState(pos.above()).isAir()) {
-                ItemEntity itemEntity = new ItemEntity(world, pos.getX() + 0.5D, pos.getY() + 1.0D, pos.getZ() + 0.5D, stack.split(1));
+                ItemEntity itemEntity = new ItemEntity(world, pos.getX() + 0.5D, pos.getY() + 1.0D, pos.getZ() + 0.5D, stack.copyWithCount(1));
                 world.addFreshEntity(itemEntity);
             } else {
-                ItemEntity itemEntity = new ItemEntity(world, pos.getX() + 0.5D, pos.getY() - 0.5D, pos.getZ() + 0.5D, stack.split(1));
+                ItemEntity itemEntity = new ItemEntity(world, pos.getX() + 0.5D, pos.getY() - 0.5D, pos.getZ() + 0.5D, stack.copyWithCount(1));
                 world.addFreshEntity(itemEntity);
             }
         }
