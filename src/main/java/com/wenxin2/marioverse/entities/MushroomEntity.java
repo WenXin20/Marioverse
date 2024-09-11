296c9e092906e342eeae8c5e380c1d3ec9e9829c
WenXin2 <dwnewville@gmail.com>
2024-09-10 22:00:32 -0500

    Increase movement speed

--- src/main/java/com/wenxin2/marioverse/entities/MushroomEntity.java
+++ src/main/java/com/wenxin2/marioverse/entities/MushroomEntity.java
@@ -36,7 +36,7 @@ public class MushroomEntity extends BaseMushroomEntity implements GeoEntity {
     @Override
     protected void registerGoals() {
         this.goalSelector.addGoal(1, new FloatGoal(this));
-        this.goalSelector.addGoal(2, new MushroomMoveGoal(this, 0.5D));
+        this.goalSelector.addGoal(2, new MushroomMoveGoal(this, 0.7D));
         this.goalSelector.addGoal(3, new LookAtPlayerGoal(this, Player.class, 8.0F));
     }
 
