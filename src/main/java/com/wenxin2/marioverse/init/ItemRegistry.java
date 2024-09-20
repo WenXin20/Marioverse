99542a83a5f3bb4131b6fdc5c5b4ca7cdb209e79
WenXin2 <dwnewville@gmail.com>
2024-09-20 00:03:20 -0500

    Add fire flower costume texture/model

--- src/main/java/com/wenxin2/marioverse/init/ItemRegistry.java
+++ src/main/java/com/wenxin2/marioverse/init/ItemRegistry.java
@@ -15,10 +15,12 @@ import net.neoforged.neoforge.registries.DeferredItem;
 public class ItemRegistry {
     public static final DeferredItem<Item> FIRE_FLOWER;
     public static final DeferredItem<Item> FIRE_FLOWER_HAT;
+    public static final DeferredItem<Item> FIRE_FLOWER_PANTS;
     public static final DeferredItem<Item> FIRE_FLOWER_SHIRT;
-    public static final DeferredItem<Item> PIPE_WRENCH;
+    public static final DeferredItem<Item> FIRE_FLOWER_SHOES;
     public static final DeferredItem<Item> MUSHROOM;
     public static final DeferredItem<Item> ONE_UP_MUSHROOM;
+    public static final DeferredItem<Item> PIPE_WRENCH;
 
     static
     {
@@ -30,12 +32,17 @@ public class ItemRegistry {
                 () -> new BasePowerUpItem(EntityRegistry.MUSHROOM, 0xFFFFFF, 0xFFFFFF, new Item.Properties()));
         ONE_UP_MUSHROOM = registerItem("one_up_mushroom",
                 () -> new OneUpMushroomItem(EntityRegistry.ONE_UP_MUSHROOM, 0xFFFFFF, 0xFFFFFF, new Item.Properties().stacksTo(8)));
+
         FIRE_FLOWER = registerItem("fire_flower",
                 () -> new BasePowerUpItem(EntityRegistry.FIRE_FLOWER, 0xFFFFFF, 0xFFFFFF, new Item.Properties()));
         FIRE_FLOWER_HAT = registerItem("fire_flower_hat",
-                () -> new FireFlowerCostumeItem(ArmorMaterials.LEATHER, ArmorItem.Type.HELMET, new Item.Properties()));
+                () -> new FireFlowerCostumeItem(ArmorMaterials.LEATHER, ArmorItem.Type.HELMET, new Item.Properties().stacksTo(1)));
         FIRE_FLOWER_SHIRT = registerItem("fire_flower_shirt",
-                () -> new FireFlowerCostumeItem(ArmorMaterials.LEATHER, ArmorItem.Type.CHESTPLATE, new Item.Properties()));
+                () -> new FireFlowerCostumeItem(ArmorMaterials.LEATHER, ArmorItem.Type.CHESTPLATE, new Item.Properties().stacksTo(1)));
+        FIRE_FLOWER_PANTS = registerItem("fire_flower_pants",
+                () -> new FireFlowerCostumeItem(ArmorMaterials.LEATHER, ArmorItem.Type.LEGGINGS, new Item.Properties().stacksTo(1)));
+        FIRE_FLOWER_SHOES = registerItem("fire_flower_shoes",
+                () -> new FireFlowerCostumeItem(ArmorMaterials.LEATHER, ArmorItem.Type.BOOTS, new Item.Properties().stacksTo(1)));
     }
 
     public static <T extends Item> DeferredItem<T> registerItem(String name, Supplier<T> item)
