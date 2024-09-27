6616a4655e6a4ba8e76c0a53149d4dbf99946ae0
WenXin2 <dwnewville@gmail.com>
2024-09-27 00:27:03 -0500

    Revert "Attempt trims layer"
    This reverts commit eb5512dbb75425c6c8f2cc412694771b2251bf5a.

--- src/main/java/com/wenxin2/marioverse/client/renderers/costumes/FireCostumeRenderer.java
+++ src/main/java/com/wenxin2/marioverse/client/renderers/costumes/FireCostumeRenderer.java
@@ -5,12 +5,10 @@ import com.wenxin2.marioverse.items.FireCostumeItem;
 import net.minecraft.resources.ResourceLocation;
 import software.bernie.geckolib.model.DefaultedItemGeoModel;
 import software.bernie.geckolib.renderer.GeoArmorRenderer;
-import software.bernie.geckolib.renderer.layer.AutoGlowingGeoLayer;
 
 public class FireCostumeRenderer extends GeoArmorRenderer<FireCostumeItem> {
     public FireCostumeRenderer() {
         super(new DefaultedItemGeoModel<>(ResourceLocation
                 .fromNamespaceAndPath(Marioverse.MOD_ID, "costume/fire_costume")));
-        addRenderLayer(new ArmorTrimGeoLayer<>(this));
     }
 }
--- src/main/java/com/wenxin2/marioverse/mixin/HumanoidArmorLayerMixin.java
+++ src/main/java/com/wenxin2/marioverse/mixin/HumanoidArmorLayerMixin.java
@@ -5,16 +5,12 @@ import com.llamalad7.mixinextras.injector.wrapoperation.WrapOperation;
 import com.mojang.blaze3d.vertex.PoseStack;
 import com.wenxin2.marioverse.client.renderers.ArmorRenderingExtension;
 import net.minecraft.client.model.HumanoidModel;
-import net.minecraft.client.model.Model;
 import net.minecraft.client.renderer.MultiBufferSource;
 import net.minecraft.client.renderer.entity.layers.HumanoidArmorLayer;
 import net.minecraft.client.renderer.texture.OverlayTexture;
-import net.minecraft.core.Holder;
 import net.minecraft.core.component.DataComponents;
 import net.minecraft.world.entity.EquipmentSlot;
 import net.minecraft.world.entity.LivingEntity;
-import net.minecraft.world.item.ArmorItem;
-import net.minecraft.world.item.ArmorMaterial;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.armortrim.ArmorTrim;
 import org.jetbrains.annotations.Nullable;
@@ -31,20 +27,10 @@ import software.bernie.geckolib.util.Color;
 public abstract class HumanoidArmorLayerMixin<T extends LivingEntity, A extends HumanoidModel<T>> implements ArmorRenderingExtension<T, A> {
 
     @Shadow
-    protected abstract void renderArmorPiece(PoseStack poseStack, MultiBufferSource multiBufferSource, T livingEntity,
-                                             EquipmentSlot equipmentSlot, int i, A humanoidModel);
-
-    @Shadow
-    protected abstract void renderTrim(Holder<ArmorMaterial> armorMaterial, PoseStack poseStack, MultiBufferSource buffer,
-                            int i, ArmorTrim trim, Model model, boolean humanoidModel);
+    protected abstract void renderArmorPiece(PoseStack poseStack, MultiBufferSource multiBufferSource, T livingEntity, EquipmentSlot equipmentSlot, int i, A humanoidModel);
 
     @Shadow
     private A getArmorModel(EquipmentSlot slot) { return null; }
-
-    @Shadow
-    private boolean usesInnerModel(EquipmentSlot slot) {
-        return slot == EquipmentSlot.LEGS;
-    }
 
     @Shadow @Final private A innerModel;
     @Unique
@@ -64,9 +50,6 @@ public abstract class HumanoidArmorLayerMixin<T extends LivingEntity, A extends 
                     light, OverlayTexture.NO_OVERLAY, Color.WHITE.argbInt());
             baseModel.copyPropertiesTo((A)geckolibModel);
             ArmorTrim armortrim = stack.get(DataComponents.TRIM);
-            if (stack.getItem() instanceof ArmorItem armoritem && armortrim != null) {
-                this.renderTrim(armoritem.getMaterial(), poseStack, multiBufferSource, light, armortrim, baseModel, this.usesInnerModel(equipmentSlot));
-            }
         } else {
             this.renderArmorPiece(poseStack, multiBufferSource, livingEntity, equipmentSlot, light, this.getArmorModel(equipmentSlot));
         }
