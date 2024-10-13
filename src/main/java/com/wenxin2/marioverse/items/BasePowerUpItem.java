43d6dd4c65ce8ddec8deb0873f9daeebdd3a2be7
WenXin2 <dwnewville@gmail.com>
2024-10-12 22:42:07 -0500

    Prevent all power ups from placing in spawners in survival

--- src/main/java/com/wenxin2/marioverse/items/BasePowerUpItem.java
+++ src/main/java/com/wenxin2/marioverse/items/BasePowerUpItem.java
@@ -1,12 +1,74 @@
 package com.wenxin2.marioverse.items;
 
+import java.util.Objects;
 import java.util.function.Supplier;
+import net.minecraft.core.BlockPos;
+import net.minecraft.core.Direction;
+import net.minecraft.server.level.ServerLevel;
+import net.minecraft.world.InteractionResult;
 import net.minecraft.world.entity.EntityType;
 import net.minecraft.world.entity.Mob;
+import net.minecraft.world.entity.MobSpawnType;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.context.UseOnContext;
+import net.minecraft.world.level.Level;
+import net.minecraft.world.level.Spawner;
+import net.minecraft.world.level.block.state.BlockState;
+import net.minecraft.world.level.gameevent.GameEvent;
 import net.neoforged.neoforge.common.DeferredSpawnEggItem;
+import org.jetbrains.annotations.NotNull;
 
 public class BasePowerUpItem extends DeferredSpawnEggItem {
     public BasePowerUpItem(Supplier<? extends EntityType<? extends Mob>> entityType, int primaryColor, int secondaryColor, Properties properties) {
         super(entityType, primaryColor, secondaryColor, properties);
+    }
+
+    @NotNull
+    @Override
+    public InteractionResult useOn(UseOnContext context) {
+        Level level = context.getLevel();
+        if (!(level instanceof ServerLevel)) {
+            return InteractionResult.SUCCESS;
+        } else {
+            ItemStack itemstack = context.getItemInHand();
+            BlockPos blockpos = context.getClickedPos();
+            Direction direction = context.getClickedFace();
+            BlockState blockstate = level.getBlockState(blockpos);
+            if (level.getBlockEntity(blockpos) instanceof Spawner spawner) {
+                if (context.getPlayer() != null && context.getPlayer().isCreative()) {
+                    EntityType<?> entitytype1 = this.getType(itemstack);
+                    spawner.setEntityId(entitytype1, level.getRandom());
+                    level.sendBlockUpdated(blockpos, blockstate, blockstate, 3);
+                    level.gameEvent(context.getPlayer(), GameEvent.BLOCK_CHANGE, blockpos);
+                    itemstack.shrink(1);
+                    return InteractionResult.CONSUME;
+                }
+                return InteractionResult.PASS;
+            } else {
+                BlockPos blockpos1;
+                if (blockstate.getCollisionShape(level, blockpos).isEmpty()) {
+                    blockpos1 = blockpos;
+                } else {
+                    blockpos1 = blockpos.relative(direction);
+                }
+
+                EntityType<?> entitytype = this.getType(itemstack);
+                if (entitytype.spawn(
+                        (ServerLevel)level,
+                        itemstack,
+                        context.getPlayer(),
+                        blockpos1,
+                        MobSpawnType.SPAWN_EGG,
+                        true,
+                        !Objects.equals(blockpos, blockpos1) && direction == Direction.UP
+                )
+                        != null) {
+                    itemstack.shrink(1);
+                    level.gameEvent(context.getPlayer(), GameEvent.ENTITY_PLACE, blockpos);
+                }
+
+                return InteractionResult.CONSUME;
+            }
+        }
     }
 }
