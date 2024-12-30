9ddab6ef3b6138604a2543eca6a9a354ec72942f
WenXin2 <dwnewville@gmail.com>
2024-12-30 11:40:37 -0600

    Increase shadow size of wider mobs

---
+++ src/main/java/com/wenxin2/marioverse/mixin/EntityRendererMixin.java
@@ -0,0 +1,25 @@
+package com.wenxin2.marioverse.mixin;
+
+import com.wenxin2.marioverse.init.AttributesRegistry;
+import net.minecraft.client.renderer.entity.EntityRenderer;
+import net.minecraft.world.entity.Entity;
+import net.minecraft.world.entity.LivingEntity;
+import net.minecraft.world.entity.ai.attributes.AttributeMap;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+
+@Mixin(EntityRenderer.class)
+public abstract class EntityRendererMixin<T extends Entity> {
+    @Inject(method = "getShadowRadius(Lnet/minecraft/world/entity/Entity;)F", at = @At("RETURN"), cancellable = true)
+    public void getShadowRadius(T entity, CallbackInfoReturnable<Float> cir) {
+        if (entity instanceof LivingEntity livingEntity) {
+            AttributeMap attributemap = livingEntity.getAttributes();
+            float widthScale = (float) attributemap.getValue(AttributesRegistry.WIDTH_SCALE);
+
+            if (widthScale != 1)
+                cir.setReturnValue(cir.getReturnValue() * widthScale / 1.25F);
+        }
+    }
+}
