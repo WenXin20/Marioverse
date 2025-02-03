af2a992090e846f579e67c8fb1f3efe58c0dec26
WenXin2 <dwnewville@gmail.com>
2025-02-03 01:00:58 -0600

    Update BlockSetTypeRegistry.java

--- src/main/java/com/wenxin2/marioverse/init/BlockSetTypeRegistry.java
+++ src/main/java/com/wenxin2/marioverse/init/BlockSetTypeRegistry.java
@@ -11,8 +11,8 @@ public class BlockSetTypeRegistry {
                     BlockSetType.PressurePlateSensitivity.MOBS, SoundType.AMETHYST,
                     SoundEvents.IRON_DOOR_CLOSE, SoundEvents.IRON_DOOR_OPEN,
                     SoundEvents.IRON_TRAPDOOR_CLOSE, SoundEvents.IRON_TRAPDOOR_OPEN,
-                    SoundEvents.STONE_PRESSURE_PLATE_CLICK_OFF, SoundEvents.STONE_PRESSURE_PLATE_CLICK_ON,
-                    SoundEvents.STONE_BUTTON_CLICK_OFF, SoundEvents.STONE_BUTTON_CLICK_ON
+                    SoundEvents.AMETHYST_BLOCK_BREAK, SoundEvents.AMETHYST_BLOCK_PLACE,
+                    SoundEvents.AMETHYST_BLOCK_BREAK, SoundEvents.AMETHYST_BLOCK_PLACE
             )
     );
 
