c9548661b5298a65ff07553280ce1504c900d45d
WenXin2 <dwnewville@gmail.com>
2025-02-09 02:21:11 -0600

    Improve recipe builder

--- src/main/java/com/wenxin2/marioverse/datagen/BlockRecipeProvider.java
+++ src/main/java/com/wenxin2/marioverse/datagen/BlockRecipeProvider.java
@@ -1,5 +1,6 @@
 package com.wenxin2.marioverse.datagen;
 
+import com.wenxin2.marioverse.Marioverse;
 import com.wenxin2.marioverse.init.BlockRegistry;
 import java.util.concurrent.CompletableFuture;
 import net.minecraft.core.HolderLookup;
@@ -24,18 +25,19 @@ public class BlockRecipeProvider extends RecipeProvider {
 
     @Override
     protected void buildRecipes(RecipeOutput output) {
-        pedestalRecipe(RecipeCategory.BUILDING_BLOCKS, BlockRegistry.COPPER_QUESTION_BLOCK, Items.CUT_COPPER, output);
+        pedestalRecipe(RecipeCategory.BUILDING_BLOCKS, BlockRegistry.CUT_COPPER_PEDESTAL, Items.CUT_COPPER, output);
         questionBlockRecipe(RecipeCategory.BUILDING_BLOCKS, BlockRegistry.COPPER_QUESTION_BLOCK, Items.COPPER_BLOCK, Tags.Items.CHESTS_WOODEN, output);
         storageBrickRecipe(RecipeCategory.BUILDING_BLOCKS, BlockRegistry.STORAGE_CUT_COPPER, Items.CUT_COPPER, Tags.Items.CHESTS_WOODEN, output);
 
-        stonecutterResultFromBase(output, RecipeCategory.BUILDING_BLOCKS, Items.COPPER_BLOCK, BlockRegistry.SMASHABLE_CUT_COPPER);
-        stonecutterResultFromBase(output, RecipeCategory.BUILDING_BLOCKS, Items.CUT_COPPER, BlockRegistry.SMASHABLE_CUT_COPPER);
-        stonecutterResultFromBase(output, RecipeCategory.BUILDING_BLOCKS, Items.COPPER_BLOCK, BlockRegistry.CUT_COPPER_PEDESTAL);
-        stonecutterResultFromBase(output, RecipeCategory.BUILDING_BLOCKS, Items.CUT_COPPER, BlockRegistry.CUT_COPPER_PEDESTAL);
+        stonecutterRecipe(output, RecipeCategory.BUILDING_BLOCKS, BlockRegistry.CUT_COPPER_PEDESTAL, Items.CUT_COPPER, 1);
+        stonecutterRecipe(output, RecipeCategory.BUILDING_BLOCKS, BlockRegistry.SMASHABLE_CUT_COPPER, Items.CUT_COPPER, 1);
+
+        stonecutterFromBaseRecipe(output, RecipeCategory.BUILDING_BLOCKS, BlockRegistry.CUT_COPPER_PEDESTAL, Items.COPPER_BLOCK, 1);
+        stonecutterFromBaseRecipe(output, RecipeCategory.BUILDING_BLOCKS, BlockRegistry.SMASHABLE_CUT_COPPER, Items.COPPER_BLOCK, 1);
     }
 
-    private void pedestalRecipe(RecipeCategory category, ItemLike itemResult, ItemLike recipeItem, RecipeOutput output) {
-        ShapedRecipeBuilder.shaped(category, itemResult)
+    private void pedestalRecipe(RecipeCategory category, ItemLike resultItem, ItemLike recipeItem, RecipeOutput output) {
+        ShapedRecipeBuilder.shaped(category, resultItem)
                 .define('B', recipeItem)
                 .pattern("B B")
                 .pattern("BBB")
@@ -43,24 +45,38 @@ public class BlockRecipeProvider extends RecipeProvider {
                 .save(output);
     }
 
-    private void questionBlockRecipe(RecipeCategory category, ItemLike itemResult, ItemLike recipeItem,
+    private void questionBlockRecipe(RecipeCategory category, ItemLike resultItem, ItemLike recipeItem,
                                      TagKey<Item> itemTag, RecipeOutput output) {
-        ShapelessRecipeBuilder.shapeless(category, itemResult)
+        ShapelessRecipeBuilder.shapeless(category, resultItem)
                 .requires(recipeItem)
                 .requires(itemTag)
                 .unlockedBy("has_block", has(recipeItem))
+                .unlockedBy("has_chest", has(itemTag))
                 .save(output);
     }
 
-    private void storageBrickRecipe(RecipeCategory category, ItemLike itemResult, ItemLike recipeItem,
+    private void storageBrickRecipe(RecipeCategory category, ItemLike resultItem, ItemLike recipeItem,
                                     TagKey<Item> itemTag, RecipeOutput output) {
-        ShapedRecipeBuilder.shaped(category, itemResult)
+        ShapedRecipeBuilder.shaped(category, resultItem)
                 .define('B', recipeItem)
                 .define('C', itemTag)
                 .pattern(" B ")
                 .pattern("BCB")
                 .pattern(" B ")
                 .unlockedBy("has_block", has(recipeItem))
+                .unlockedBy("has_chest", has(itemTag))
                 .save(output);
+    }
+
+    protected void stonecutterRecipe(RecipeOutput output, RecipeCategory category, ItemLike resultItem, ItemLike recipeItem, int outputAmt) {
+        SingleItemRecipeBuilder.stonecutting(Ingredient.of(recipeItem), category, resultItem, outputAmt)
+                .unlockedBy(getHasName(recipeItem), has(recipeItem))
+                .save(output, Marioverse.MOD_ID + ":" + getSimpleRecipeName(resultItem) + "_stonecutting");
+    }
+
+    protected void stonecutterFromBaseRecipe(RecipeOutput output, RecipeCategory category, ItemLike resultItem, ItemLike recipeItem, int outputAmt) {
+        SingleItemRecipeBuilder.stonecutting(Ingredient.of(recipeItem), category, resultItem, outputAmt)
+                .unlockedBy(getHasName(recipeItem), has(recipeItem))
+                .save(output, Marioverse.MOD_ID + ":" + getConversionRecipeName(resultItem, recipeItem) + "_stonecutting");
     }
 }
