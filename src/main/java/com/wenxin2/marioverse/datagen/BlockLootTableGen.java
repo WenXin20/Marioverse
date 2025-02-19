d9cd5dc01490765ecce96665beb1d1842a567ad5
WenXin2 <dwnewville@gmail.com>
2025-02-19 15:38:02 -0600

    Gen loot for goal poles & pipes

--- src/main/java/com/wenxin2/marioverse/datagen/BlockLootTableGen.java
+++ src/main/java/com/wenxin2/marioverse/datagen/BlockLootTableGen.java
@@ -1,6 +1,8 @@
 package com.wenxin2.marioverse.datagen;
 
 import com.wenxin2.marioverse.Marioverse;
+import com.wenxin2.marioverse.blocks.GoalPoleBlock;
+import com.wenxin2.marioverse.blocks.WarpPipeBlock;
 import com.wenxin2.marioverse.data.BlockFamilyExtended;
 import com.wenxin2.marioverse.init.BlockFamilyRegistry;
 import java.util.List;
@@ -8,13 +10,19 @@ import java.util.Map;
 import java.util.Set;
 import java.util.concurrent.CompletableFuture;
 import net.minecraft.core.HolderLookup;
+import net.minecraft.core.component.DataComponents;
 import net.minecraft.data.PackOutput;
 import net.minecraft.data.loot.BlockLootSubProvider;
 import net.minecraft.data.loot.LootTableProvider;
 import net.minecraft.world.flag.FeatureFlags;
 import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.storage.loot.BuiltInLootTables;
+import net.minecraft.world.level.storage.loot.LootPool;
+import net.minecraft.world.level.storage.loot.LootTable;
+import net.minecraft.world.level.storage.loot.entries.LootItem;
+import net.minecraft.world.level.storage.loot.functions.CopyComponentsFunction;
 import net.minecraft.world.level.storage.loot.parameters.LootContextParamSets;
+import net.minecraft.world.level.storage.loot.providers.number.ConstantValue;
 import net.neoforged.neoforge.registries.DeferredHolder;
 import org.jetbrains.annotations.NotNull;
 
@@ -43,6 +51,8 @@ public class BlockLootTableGen extends LootTableProvider {
                 if (block.getLootTable() != BuiltInLootTables.EMPTY) {
                     if (isBlockInVariants(block))
                         this.genBlockVariants(block);
+                    else if (block instanceof GoalPoleBlock || block instanceof WarpPipeBlock)
+                        this.add(block, this.createNameableBlockEntityTable(block));
                     else this.dropSelf(block);
                 }
             });
@@ -65,10 +75,19 @@ public class BlockLootTableGen extends LootTableProvider {
                     BlockFamilyExtended.Variant smashableBlocks = BlockFamilyExtended.Variant.SMASHABLE_BLOCKS;
 
                     if (variant == smashableBlocks)
-                        add(variantBlock, createSilkTouchOnlyTable(variantBlock));
+                        add(variantBlock, this.createSilkTouchOnlyTable(variantBlock));
                     else dropSelf(variantBlock);
                 });
             });
+        }
+
+        protected LootTable.Builder createNameableBlockEntityTable(Block block) {
+            return LootTable.lootTable().withPool(this.applyExplosionCondition(block,
+                    LootPool.lootPool().setRolls(ConstantValue.exactly(1.0F))
+                            .add(LootItem.lootTableItem(block)
+                                    .apply(CopyComponentsFunction.copyComponents(CopyComponentsFunction.Source.BLOCK_ENTITY)
+                                            .include(DataComponents.CUSTOM_NAME))))
+            );
         }
     }
 }
