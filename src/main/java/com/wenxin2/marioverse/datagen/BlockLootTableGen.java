09e0da16799e41bc19f08702dc7b05a558501332
WenXin2 <dwnewville@gmail.com>
2025-02-19 13:58:30 -0600

    Add loot gen method for all blocks

--- src/main/java/com/wenxin2/marioverse/datagen/BlockLootTableGen.java
+++ src/main/java/com/wenxin2/marioverse/datagen/BlockLootTableGen.java
@@ -1,8 +1,10 @@
 package com.wenxin2.marioverse.datagen;
 
-import com.wenxin2.marioverse.init.BlockRegistry;
-import java.util.HashSet;
+import com.wenxin2.marioverse.Marioverse;
+import com.wenxin2.marioverse.data.BlockFamilyExtended;
+import com.wenxin2.marioverse.init.BlockFamilyRegistry;
 import java.util.List;
+import java.util.Map;
 import java.util.Set;
 import java.util.concurrent.CompletableFuture;
 import net.minecraft.core.HolderLookup;
@@ -11,18 +13,17 @@ import net.minecraft.data.loot.BlockLootSubProvider;
 import net.minecraft.data.loot.LootTableProvider;
 import net.minecraft.world.flag.FeatureFlags;
 import net.minecraft.world.level.block.Block;
+import net.minecraft.world.level.storage.loot.BuiltInLootTables;
 import net.minecraft.world.level.storage.loot.parameters.LootContextParamSets;
+import net.neoforged.neoforge.registries.DeferredHolder;
 import org.jetbrains.annotations.NotNull;
 
 public class BlockLootTableGen extends LootTableProvider {
-
     public BlockLootTableGen(PackOutput output, CompletableFuture<HolderLookup.Provider> completableFuture) {
         super(output, Set.of(), List.of(new LootTableProvider.SubProviderEntry(BlockSubProvider::new, LootContextParamSets.BLOCK)), completableFuture);
     }
 
     public static class BlockSubProvider extends BlockLootSubProvider {
-        private final Set<Block> generatedBlocks = new HashSet<>();
-
         public BlockSubProvider(HolderLookup.Provider provider) {
             super(Set.of(), FeatureFlags.DEFAULT_FLAGS, provider);
         }
@@ -30,108 +31,44 @@ public class BlockLootTableGen extends LootTableProvider {
         @NotNull
         @Override
         protected Iterable<Block> getKnownBlocks() {
-            return generatedBlocks;
+            return Marioverse.BLOCKS.getEntries().stream().map(DeferredHolder::value)
+                    .filter(value -> value instanceof Block)
+                    .map(value -> (Block) value).toList();
         }
 
         @Override
         protected void generate() {
-            generatedBlocks.add(BlockRegistry.COPPER_QUESTION_BLOCK.get());
-            generatedBlocks.add(BlockRegistry.CUT_COPPER_PEDESTAL.get());
-            generatedBlocks.add(BlockRegistry.INVISIBLE_COPPER_QUESTION_BLOCK.get());
-            generatedBlocks.add(BlockRegistry.SMASHABLE_CUT_COPPER.get());
-            generatedBlocks.add(BlockRegistry.STORAGE_CUT_COPPER.get());
+            Marioverse.BLOCKS.getEntries().forEach(deferredHolder -> {
+                Block block = deferredHolder.get();
+                if (block.getLootTable() != BuiltInLootTables.EMPTY) {
+                    if (isBlockInVariants(block))
+                        this.genBlockVariants(block);
+                    else this.dropSelf(block);
+                }
+            });
+        }
 
-            generatedBlocks.add(BlockRegistry.EXPOSED_COPPER_QUESTION_BLOCK.get());
-            generatedBlocks.add(BlockRegistry.EXPOSED_CUT_COPPER_PEDESTAL.get());
-            generatedBlocks.add(BlockRegistry.INVISIBLE_EXPOSED_COPPER_QUESTION_BLOCK.get());
-            generatedBlocks.add(BlockRegistry.SMASHABLE_EXPOSED_CUT_COPPER.get());
-            generatedBlocks.add(BlockRegistry.STORAGE_EXPOSED_CUT_COPPER.get());
+        private boolean isBlockInVariants(Block block) {
+            // Iterate over all BlockFamilies
+            for (BlockFamilyExtended blockFamily : BlockFamilyRegistry.getAllExtendedFamilies().toList()) {
+                for (Map.Entry<BlockFamilyExtended.Variant, Block> entry : blockFamily.getVariants().entrySet()) {
+                    if (entry.getValue().equals(block))
+                        return true;
+                }
+            }
+            return false;
+        }
 
-            generatedBlocks.add(BlockRegistry.WEATHERED_COPPER_QUESTION_BLOCK.get());
-            generatedBlocks.add(BlockRegistry.WEATHERED_CUT_COPPER_PEDESTAL.get());
-            generatedBlocks.add(BlockRegistry.INVISIBLE_WEATHERED_COPPER_QUESTION_BLOCK.get());
-            generatedBlocks.add(BlockRegistry.SMASHABLE_WEATHERED_CUT_COPPER.get());
-            generatedBlocks.add(BlockRegistry.STORAGE_WEATHERED_CUT_COPPER.get());
+        private void genBlockVariants(Block block) {
+            BlockFamilyRegistry.getAllExtendedFamilies().forEach(blockFamily -> {
+                blockFamily.getVariants().forEach((variant, variantBlock) -> {
+                    BlockFamilyExtended.Variant smashableBlocks = BlockFamilyExtended.Variant.SMASHABLE_BLOCKS;
 
-            generatedBlocks.add(BlockRegistry.OXIDIZED_COPPER_QUESTION_BLOCK.get());
-            generatedBlocks.add(BlockRegistry.OXIDIZED_CUT_COPPER_PEDESTAL.get());
-            generatedBlocks.add(BlockRegistry.INVISIBLE_OXIDIZED_COPPER_QUESTION_BLOCK.get());
-            generatedBlocks.add(BlockRegistry.SMASHABLE_OXIDIZED_CUT_COPPER.get());
-            generatedBlocks.add(BlockRegistry.STORAGE_OXIDIZED_CUT_COPPER.get());
-
-            generatedBlocks.add(BlockRegistry.WAXED_COPPER_QUESTION_BLOCK.get());
-            generatedBlocks.add(BlockRegistry.WAXED_CUT_COPPER_PEDESTAL.get());
-            generatedBlocks.add(BlockRegistry.INVISIBLE_WAXED_COPPER_QUESTION_BLOCK.get());
-            generatedBlocks.add(BlockRegistry.SMASHABLE_WAXED_CUT_COPPER.get());
-            generatedBlocks.add(BlockRegistry.STORAGE_WAXED_CUT_COPPER.get());
-
-            generatedBlocks.add(BlockRegistry.WAXED_EXPOSED_COPPER_QUESTION_BLOCK.get());
-            generatedBlocks.add(BlockRegistry.WAXED_EXPOSED_CUT_COPPER_PEDESTAL.get());
-            generatedBlocks.add(BlockRegistry.INVISIBLE_WAXED_EXPOSED_COPPER_QUESTION_BLOCK.get());
-            generatedBlocks.add(BlockRegistry.SMASHABLE_WAXED_EXPOSED_CUT_COPPER.get());
-            generatedBlocks.add(BlockRegistry.STORAGE_WAXED_EXPOSED_CUT_COPPER.get());
-
-            generatedBlocks.add(BlockRegistry.WAXED_WEATHERED_COPPER_QUESTION_BLOCK.get());
-            generatedBlocks.add(BlockRegistry.WAXED_WEATHERED_CUT_COPPER_PEDESTAL.get());
-            generatedBlocks.add(BlockRegistry.INVISIBLE_WAXED_WEATHERED_COPPER_QUESTION_BLOCK.get());
-            generatedBlocks.add(BlockRegistry.SMASHABLE_WAXED_WEATHERED_CUT_COPPER.get());
-            generatedBlocks.add(BlockRegistry.STORAGE_WAXED_WEATHERED_CUT_COPPER.get());
-
-            generatedBlocks.add(BlockRegistry.WAXED_OXIDIZED_COPPER_QUESTION_BLOCK.get());
-            generatedBlocks.add(BlockRegistry.WAXED_OXIDIZED_CUT_COPPER_PEDESTAL.get());
-            generatedBlocks.add(BlockRegistry.INVISIBLE_WAXED_OXIDIZED_COPPER_QUESTION_BLOCK.get());
-            generatedBlocks.add(BlockRegistry.SMASHABLE_WAXED_OXIDIZED_CUT_COPPER.get());
-            generatedBlocks.add(BlockRegistry.STORAGE_WAXED_OXIDIZED_CUT_COPPER.get());
-
-            add(BlockRegistry.SMASHABLE_CUT_COPPER.get(), createSilkTouchOnlyTable(BlockRegistry.SMASHABLE_CUT_COPPER.get()));
-            add(BlockRegistry.SMASHABLE_EXPOSED_CUT_COPPER.get(), createSilkTouchOnlyTable(BlockRegistry.SMASHABLE_EXPOSED_CUT_COPPER.get()));
-            add(BlockRegistry.SMASHABLE_WEATHERED_CUT_COPPER.get(), createSilkTouchOnlyTable(BlockRegistry.SMASHABLE_WEATHERED_CUT_COPPER.get()));
-            add(BlockRegistry.SMASHABLE_OXIDIZED_CUT_COPPER.get(), createSilkTouchOnlyTable(BlockRegistry.SMASHABLE_OXIDIZED_CUT_COPPER.get()));
-
-            add(BlockRegistry.SMASHABLE_WAXED_CUT_COPPER.get(), createSilkTouchOnlyTable(BlockRegistry.SMASHABLE_WAXED_CUT_COPPER.get()));
-            add(BlockRegistry.SMASHABLE_WAXED_EXPOSED_CUT_COPPER.get(), createSilkTouchOnlyTable(BlockRegistry.SMASHABLE_WAXED_EXPOSED_CUT_COPPER.get()));
-            add(BlockRegistry.SMASHABLE_WAXED_WEATHERED_CUT_COPPER.get(), createSilkTouchOnlyTable(BlockRegistry.SMASHABLE_WAXED_WEATHERED_CUT_COPPER.get()));
-            add(BlockRegistry.SMASHABLE_WAXED_OXIDIZED_CUT_COPPER.get(), createSilkTouchOnlyTable(BlockRegistry.SMASHABLE_WAXED_OXIDIZED_CUT_COPPER.get()));
-
-            dropSelf(BlockRegistry.COPPER_QUESTION_BLOCK.get());
-            dropSelf(BlockRegistry.CUT_COPPER_PEDESTAL.get());
-            dropSelf(BlockRegistry.INVISIBLE_COPPER_QUESTION_BLOCK.get());
-            dropSelf(BlockRegistry.STORAGE_CUT_COPPER.get());
-
-            dropSelf(BlockRegistry.EXPOSED_COPPER_QUESTION_BLOCK.get());
-            dropSelf(BlockRegistry.EXPOSED_CUT_COPPER_PEDESTAL.get());
-            dropSelf(BlockRegistry.INVISIBLE_EXPOSED_COPPER_QUESTION_BLOCK.get());
-            dropSelf(BlockRegistry.STORAGE_EXPOSED_CUT_COPPER.get());
-
-            dropSelf(BlockRegistry.WEATHERED_COPPER_QUESTION_BLOCK.get());
-            dropSelf(BlockRegistry.WEATHERED_CUT_COPPER_PEDESTAL.get());
-            dropSelf(BlockRegistry.INVISIBLE_WEATHERED_COPPER_QUESTION_BLOCK.get());
-            dropSelf(BlockRegistry.STORAGE_WEATHERED_CUT_COPPER.get());
-
-            dropSelf(BlockRegistry.OXIDIZED_COPPER_QUESTION_BLOCK.get());
-            dropSelf(BlockRegistry.OXIDIZED_CUT_COPPER_PEDESTAL.get());
-            dropSelf(BlockRegistry.INVISIBLE_OXIDIZED_COPPER_QUESTION_BLOCK.get());
-            dropSelf(BlockRegistry.STORAGE_OXIDIZED_CUT_COPPER.get());
-
-            dropSelf(BlockRegistry.WAXED_COPPER_QUESTION_BLOCK.get());
-            dropSelf(BlockRegistry.WAXED_CUT_COPPER_PEDESTAL.get());
-            dropSelf(BlockRegistry.INVISIBLE_WAXED_COPPER_QUESTION_BLOCK.get());
-            dropSelf(BlockRegistry.STORAGE_WAXED_CUT_COPPER.get());
-
-            dropSelf(BlockRegistry.WAXED_EXPOSED_COPPER_QUESTION_BLOCK.get());
-            dropSelf(BlockRegistry.WAXED_EXPOSED_CUT_COPPER_PEDESTAL.get());
-            dropSelf(BlockRegistry.INVISIBLE_WAXED_EXPOSED_COPPER_QUESTION_BLOCK.get());
-            dropSelf(BlockRegistry.STORAGE_WAXED_EXPOSED_CUT_COPPER.get());
-
-            dropSelf(BlockRegistry.WAXED_WEATHERED_COPPER_QUESTION_BLOCK.get());
-            dropSelf(BlockRegistry.WAXED_WEATHERED_CUT_COPPER_PEDESTAL.get());
-            dropSelf(BlockRegistry.INVISIBLE_WAXED_WEATHERED_COPPER_QUESTION_BLOCK.get());
-            dropSelf(BlockRegistry.STORAGE_WAXED_WEATHERED_CUT_COPPER.get());
-
-            dropSelf(BlockRegistry.WAXED_OXIDIZED_COPPER_QUESTION_BLOCK.get());
-            dropSelf(BlockRegistry.WAXED_OXIDIZED_CUT_COPPER_PEDESTAL.get());
-            dropSelf(BlockRegistry.INVISIBLE_WAXED_OXIDIZED_COPPER_QUESTION_BLOCK.get());
-            dropSelf(BlockRegistry.STORAGE_WAXED_OXIDIZED_CUT_COPPER.get());
+                    if (variant == smashableBlocks)
+                        add(variantBlock, createSilkTouchOnlyTable(variantBlock));
+                    else dropSelf(variantBlock);
+                });
+            });
         }
     }
 }
