7b13d96d4b8c541b6d6858aa30b8051a32e56c92
WenXin2 <dwnewville@gmail.com>
2025-03-30 00:49:13 -0500

    Fix waist rendering

--- src/main/java/com/wenxin2/marioverse/client/renderers/costumes/MarioCostumeRenderer.java
+++ src/main/java/com/wenxin2/marioverse/client/renderers/costumes/MarioCostumeRenderer.java
@@ -3,6 +3,7 @@ package com.wenxin2.marioverse.client.renderers.costumes;
 import com.wenxin2.marioverse.Marioverse;
 import com.wenxin2.marioverse.items.MarioCostumeItem;
 import net.minecraft.resources.ResourceLocation;
+import net.minecraft.world.entity.EquipmentSlot;
 import software.bernie.geckolib.model.DefaultedItemGeoModel;
 import software.bernie.geckolib.renderer.GeoArmorRenderer;
 
@@ -10,5 +11,31 @@ public class MarioCostumeRenderer extends GeoArmorRenderer<MarioCostumeItem> {
     public MarioCostumeRenderer() {
         super(new DefaultedItemGeoModel<>(ResourceLocation
                 .fromNamespaceAndPath(Marioverse.MOD_ID, "costume/mario_costume")));
+    }
+
+    @Override
+    protected void applyBoneVisibilityBySlot(EquipmentSlot slot) {
+        this.getGeoModel().getBone("armorWaist").ifPresent(bone -> bone.setHidden(true));
+
+        switch (currentSlot) {
+            case HEAD:
+                this.setBoneVisible(this.head, true);
+                break;
+            case CHEST:
+                this.setBoneVisible(this.body, true);
+                this.setBoneVisible(this.rightArm, true);
+                this.setBoneVisible(this.leftArm, true);
+                break;
+            case LEGS:
+                this.getGeoModel().getBone("armorWaist").ifPresent(bone -> bone.setHidden(false));
+                this.setBoneVisible(this.rightLeg, true);
+                this.setBoneVisible(this.leftLeg, true);
+                break;
+            case FEET:
+                this.setBoneVisible(this.rightBoot, true);
+                this.setBoneVisible(this.leftBoot, true);
+        }
+
+        super.applyBoneVisibilityBySlot(slot);
     }
 }
