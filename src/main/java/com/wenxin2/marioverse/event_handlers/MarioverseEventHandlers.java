58325c1865a19d8d0b9f0fd1ded5c3f067fcbbc7
WenXin2 <dwnewville@gmail.com>
2025-03-30 16:49:11 -0500

    Fix tag

--- src/main/java/com/wenxin2/marioverse/event_handlers/MarioverseEventHandlers.java
+++ src/main/java/com/wenxin2/marioverse/event_handlers/MarioverseEventHandlers.java
@@ -105,10 +105,10 @@ public class MarioverseEventHandlers {
                     || ConfigRegistry.FIRE_FLOWER_POWERS_ALL_MOBS.get()))
             tag.putInt("marioverse:fireball_count", 0);
 
-        if (!tag.contains("marioverse:ice_flower")
+        if (!tag.contains("marioverse:has_ice_flower")
                 && (entity.getType().is(TagRegistry.CAN_CONSUME_ICE_FLOWERS)
                 || ConfigRegistry.ICE_FLOWER_POWERS_ALL_MOBS.get()))
-            tag.putBoolean("marioverse:ice_flower", false);
+            tag.putBoolean("marioverse:has_ice_flower", false);
 
         if (!tag.contains("marioverse:ice_ball_ready")
                 && (entity.getType().is(TagRegistry.CAN_CONSUME_ICE_FLOWERS)
