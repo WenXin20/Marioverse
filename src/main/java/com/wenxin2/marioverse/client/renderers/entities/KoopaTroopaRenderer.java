da0c6b4fc9e39063b5b0d43a7bb8ffa0a174a2fb
WenXin2 <dwnewville@gmail.com>
2025-04-13 15:52:28 -0500

    Fix shield position

--- src/main/java/com/wenxin2/marioverse/client/renderers/entities/KoopaTroopaRenderer.java
+++ src/main/java/com/wenxin2/marioverse/client/renderers/entities/KoopaTroopaRenderer.java
@@ -137,16 +137,16 @@ public class KoopaTroopaRenderer extends DynamicGeoEntityRenderer<KoopaTroopaEnt
                                               MultiBufferSource bufferSource, float partialTick, int packedLight, int packedOverlay) {
                 if (stack == KoopaTroopaRenderer.this.mainHandItem) {
                     poseStack.mulPose(Axis.XP.rotationDegrees(-90f));
-                    poseStack.translate(0, 0.125, -0.5);
+                    poseStack.translate(0.0, 0.125, -0.5);
 
                     if (stack.getItem() instanceof ShieldItem)
-                        poseStack.translate(0, 0.125, -0.05);
+                        poseStack.translate(-0.05, 0.0, 0.4);
                 } else if (stack == KoopaTroopaRenderer.this.offhandItem) {
                     poseStack.mulPose(Axis.XP.rotationDegrees(-90f));
-                    poseStack.translate(0, 0.125, -0.5);
+                    poseStack.translate(0.0, 0.125, -0.5);
 
                     if (stack.getItem() instanceof ShieldItem) {
-                        poseStack.translate(0, 0.125, 0.45);
+                        poseStack.translate(-0.05, 0.0, 0.4);
                         poseStack.mulPose(Axis.YP.rotationDegrees(180));
                     }
                 } else {
