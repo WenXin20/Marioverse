e9736be7e1233c99a2f72ae93d3ef4fb6cc26593
WenXin2 <dwnewville@gmail.com>
2025-04-15 23:03:14 -0500

    Prevent despawning in peaceful

--- src/main/java/com/wenxin2/marioverse/entities/KoopaShellEntity.java
+++ src/main/java/com/wenxin2/marioverse/entities/KoopaShellEntity.java
@@ -34,6 +34,7 @@ import net.minecraft.sounds.SoundEvent;
 import net.minecraft.sounds.SoundEvents;
 import net.minecraft.sounds.SoundSource;
 import net.minecraft.util.Mth;
+import net.minecraft.util.RandomSource;
 import net.minecraft.world.InteractionHand;
 import net.minecraft.world.InteractionResult;
 import net.minecraft.world.damagesource.DamageSource;
@@ -42,6 +43,7 @@ import net.minecraft.world.entity.Entity;
 import net.minecraft.world.entity.EntityType;
 import net.minecraft.world.entity.EquipmentSlot;
 import net.minecraft.world.entity.LivingEntity;
+import net.minecraft.world.entity.MobSpawnType;
 import net.minecraft.world.entity.TraceableEntity;
 import net.minecraft.world.entity.ai.attributes.Attribute;
 import net.minecraft.world.entity.ai.attributes.AttributeInstance;
@@ -52,6 +54,7 @@ import net.minecraft.world.entity.monster.Monster;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.level.Level;
+import net.minecraft.world.level.ServerLevelAccessor;
 import net.minecraft.world.level.block.state.BlockState;
 import net.minecraft.world.phys.AABB;
 import net.minecraft.world.phys.Vec3;
@@ -337,6 +340,12 @@ public class KoopaShellEntity extends Monster implements CrackableEntity, GeoEnt
         super.die(source);
     }
 
+    public static boolean checkKoopaSpawnRules(EntityType<? extends Monster> entityType, ServerLevelAccessor serverWorld,
+                                               MobSpawnType spawnType, BlockPos pos, RandomSource random) {
+        return (MobSpawnType.ignoresLightRequirements(spawnType) || isDarkEnoughToSpawn(serverWorld, pos, random))
+                && checkMobSpawnRules(entityType, serverWorld, spawnType, pos, random);
+    }
+
     @Override
     public int getAmbientSoundInterval() {
         return 120;
@@ -355,6 +364,11 @@ public class KoopaShellEntity extends Monster implements CrackableEntity, GeoEnt
     @Override
     public @NotNull AABB makeBoundingBox() {
         return super.makeBoundingBox();
+    }
+
+    @Override
+    protected boolean shouldDespawnInPeaceful() {
+        return false;
     }
 
     public void setOwner(@Nullable Entity ownerEntity) {
