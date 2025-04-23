fee3a7237d58b7fa149527afc9ba9cb43d058113
WenXin2 <dwnewville@gmail.com>
2025-04-23 13:32:45 -0500

    Add hurt goal

--- src/main/java/com/wenxin2/marioverse/entities/RedKoopaShellEntity.java
+++ src/main/java/com/wenxin2/marioverse/entities/RedKoopaShellEntity.java
@@ -17,6 +17,7 @@ import net.minecraft.world.entity.Entity;
 import net.minecraft.world.entity.EntityType;
 import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.entity.TraceableEntity;
+import net.minecraft.world.entity.ai.goal.target.HurtByTargetGoal;
 import net.minecraft.world.entity.monster.Monster;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.ItemStack;
@@ -30,6 +31,11 @@ public class RedKoopaShellEntity extends KoopaShellEntity implements CrackableEn
 
     public RedKoopaShellEntity(EntityType<? extends RedKoopaShellEntity> type, Level world) {
         super(type, world);
+    }
+
+    @Override
+    protected void registerGoals() {
+        this.targetSelector.addGoal(0, new HurtByTargetGoal(this).setAlertOthers(RedKoopaTroopaEntity.class));
     }
 
     @Override
