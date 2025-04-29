b0ef7f22560be3091a90f79d18d3c32a5d825b19
WenXin2 <dwnewville@gmail.com>
2025-04-29 11:46:05 -0500

    Update GoldKoopaShellEntity.java

--- src/main/java/com/wenxin2/marioverse/entities/GoldKoopaShellEntity.java
+++ src/main/java/com/wenxin2/marioverse/entities/GoldKoopaShellEntity.java
@@ -33,7 +33,8 @@ public class GoldKoopaShellEntity extends KoopaShellEntity implements CrackableE
         super.tick();
         Level world = this.level();
 
-        if (!world.isClientSide && world.getBlockState(this.blockPosition()).canBeReplaced() && coinCount <= ConfigRegistry.MAX_GOLD_KOOPA_SHELL_COINS.get()
+        if (!world.isClientSide && world.getBlockState(this.blockPosition()).canBeReplaced()
+                && coinCount <= ConfigRegistry.MAX_GOLD_KOOPA_SHELL_COINS.get()
                 && this.getDeltaMovement().horizontalDistance() > 0.25 && this.isAlive()) {
             world.setBlock(this.blockPosition(), BlockRegistry.COIN.get().defaultBlockState(), 3);
             coinCount++;
