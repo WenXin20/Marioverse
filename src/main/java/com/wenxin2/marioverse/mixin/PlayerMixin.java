e0e02824fb091c87ac6b3dee3315e33d7912045a
WenXin2 <dwnewville@gmail.com>
2025-05-10 14:06:14 -0500

    Update PlayerMixin.java

--- src/main/java/com/wenxin2/marioverse/mixin/PlayerMixin.java
+++ src/main/java/com/wenxin2/marioverse/mixin/PlayerMixin.java
@@ -1,27 +1,18 @@
 package com.wenxin2.marioverse.mixin;
 
-import com.wenxin2.marioverse.blocks.WarpPipeBlock;
 import com.wenxin2.marioverse.registries.ConfigRegistry;
-import com.wenxin2.marioverse.utils.BlockWarpEntityHandler;
 import com.wenxin2.marioverse.utils.BlockWarpPlayerHandler;
-import com.wenxin2.marioverse.utils.EntityWarpEntityHandler;
 import com.wenxin2.marioverse.utils.EntityWarpPlayerHandler;
-import net.minecraft.core.BlockPos;
-import net.minecraft.core.Direction;
-import net.minecraft.network.chat.Component;
 import net.minecraft.world.entity.Entity;
 import net.minecraft.world.entity.EntityType;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.level.Level;
-import net.minecraft.world.level.block.state.BlockState;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
 
 @Mixin(Player.class)
 public abstract class PlayerMixin extends Entity implements BlockWarpPlayerHandler, EntityWarpPlayerHandler {
     @Shadow protected abstract float getBlockSpeedFactor();
-
-    @Shadow public abstract void displayClientMessage(Component component, boolean isAboveHotbar);
 
     public PlayerMixin(EntityType<?> entityType, Level world) {
         super(entityType, world);
