bbcc683d172a31b88c2e2426843562ac1644d15c
WenXin2 <dwnewville@gmail.com>
2025-05-27 23:56:24 -0500

    Fix ice flower class

--- src/main/java/com/wenxin2/marioverse/utils/PowerUpHandler.java
+++ src/main/java/com/wenxin2/marioverse/utils/PowerUpHandler.java
@@ -3,4 +3,7 @@ package com.wenxin2.marioverse.utils;
 public interface PowerUpHandler {
     boolean mv$hasFireFlower();
     void mv$setFireFlower(boolean hasFireFlower);
+
+    boolean mv$hasIceFlower();
+    void mv$setIceFlower(boolean hasFireFlower);
 }
