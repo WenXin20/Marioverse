fa84456b9460d883b1d2eab22005e864ed718b65
WenXin2 <dwnewville@gmail.com>
2025-06-04 16:56:05 -0500

    Update LivingEntityMixin.java

--- src/main/java/com/wenxin2/marioverse/mixin/LivingEntityMixin.java
+++ src/main/java/com/wenxin2/marioverse/mixin/LivingEntityMixin.java
@@ -128,17 +128,23 @@ public abstract class LivingEntityMixin extends Entity implements BlockWarpEntit
     public void addAdditionalSaveData(CompoundTag tag, CallbackInfo ci) {
         LivingEntity entity = (LivingEntity) (Object) this;
 
-        tag.putBoolean("marioverse:has_super_star", this.mv$hasSuperStar());
         tag.putBoolean("marioverse:prevent_warp", this.mv$doPreventWarp());
-
         tag.putInt("marioverse:checkpoint_flag_cooldown", this.mv$getCheckpointFlagCooldown());
         tag.putInt("marioverse:consecutive_bounces", this.mv$getConsecutiveBounces());
+        tag.putInt("marioverse:freeze_immunity_cooldown", this.mv$getFreezeImmunityCooldown());
+        tag.putInt("marioverse:frozen_cooldown", this.mv$getFrozenCooldown());
+        tag.putInt("marioverse:prevent_warp_cooldown", this.mv$getPreventWarpCooldown());
+        tag.putInt("marioverse:warp_cooldown", this.mv$getWarpCooldown());
 
         if (entity.getType().is(TagRegistry.CAN_CONSUME_MUSHROOMS)
                 || ConfigRegistry.MUSHROOM_POWERS_ALL_MOBS.get()) {
             tag.putBoolean("marioverse:has_mega_mushroom", this.mv$hasMegaMushroom());
             tag.putBoolean("marioverse:has_mushroom", this.mv$hasMushroom());
         }
+
+        if (entity.getType().is(TagRegistry.CAN_CONSUME_ONE_UPS)
+                || ConfigRegistry.ONE_UP_HEALS_ALL_MOBS.get())
+            tag.putInt("marioverse:one_ups_rewarded", this.mv$getOneUpsRewarded());
 
         if (entity.getType().is(TagRegistry.CAN_CONSUME_FIRE_FLOWERS)
             || ConfigRegistry.FIRE_FLOWER_POWERS_ALL_MOBS.get()) {
@@ -153,28 +159,35 @@ public abstract class LivingEntityMixin extends Entity implements BlockWarpEntit
             tag.putInt("marioverse:ice_ball_cooldown", this.mv$getIceBallCooldown());
             tag.putInt("marioverse:ice_ball_count", this.mv$getIceBallCount());
         }
-        tag.putInt("marioverse:freeze_immunity_cooldown", this.mv$getFreezeImmunityCooldown());
-        tag.putInt("marioverse:frozen_cooldown", this.mv$getFrozenCooldown());
-        tag.putInt("marioverse:one_ups_rewarded", this.mv$getOneUpsRewarded());
-        tag.putInt("marioverse:prevent_warp_cooldown", this.mv$getPreventWarpCooldown());
-        tag.putInt("marioverse:super_star_cooldown", this.mv$getSuperStarCooldown());
-        tag.putInt("marioverse:warp_cooldown", this.mv$getWarpCooldown());
+
+        if (entity.getType().is(TagRegistry.CAN_CONSUME_SUPER_STARS)
+                || ConfigRegistry.SUPER_STAR_POWERS_ALL_MOBS.get()) {
+            tag.putBoolean("marioverse:has_super_star", this.mv$hasSuperStar());
+            tag.putInt("marioverse:super_star_cooldown", this.mv$getSuperStarCooldown());
+        }
     }
 
     @Inject(method = "readAdditionalSaveData", at = @At("TAIL"))
     public void readAdditionalSaveData(CompoundTag tag, CallbackInfo ci) {
         LivingEntity entity = (LivingEntity) (Object) this;
-        this.mv$setSuperStar(tag.getBoolean("marioverse:has_super_star"));
-        this.mv$setPreventWarp(tag.getBoolean("marioverse:prevent_warp"));
 
+        this.mv$setPreventWarp(tag.getBoolean("marioverse:prevent_warp"));
         this.mv$setCheckpointFlagCooldown(tag.getInt("marioverse:checkpoint_flag_cooldown"));
         this.mv$setConsecutiveBounces(tag.getInt("marioverse:consecutive_bounces"));
+        this.mv$setFreezeImmunityCooldown(tag.getInt("marioverse:freeze_immunity_cooldown"));
+        this.mv$setFrozenCooldown(tag.getInt("marioverse:frozen_cooldown"));
+        this.mv$setPreventWarpCooldown(tag.getInt("marioverse:prevent_warp_cooldown"));
+        this.mv$setWarpCooldown(tag.getInt("marioverse:warp_cooldown"));
 
         if (entity.getType().is(TagRegistry.CAN_CONSUME_MUSHROOMS)
                 || ConfigRegistry.MUSHROOM_POWERS_ALL_MOBS.get()) {
             this.mv$setMegaMushroom(tag.getBoolean("marioverse:has_mega_mushroom"));
             this.mv$setMushroom(tag.getBoolean("marioverse:has_mushroom"));
         }
+
+        if (entity.getType().is(TagRegistry.CAN_CONSUME_ONE_UPS)
+                || ConfigRegistry.ONE_UP_HEALS_ALL_MOBS.get())
+            this.mv$setOneUpsRewarded(tag.getInt("marioverse:one_ups_rewarded"));
 
         if (entity.getType().is(TagRegistry.CAN_CONSUME_FIRE_FLOWERS)
                 || ConfigRegistry.FIRE_FLOWER_POWERS_ALL_MOBS.get()) {
@@ -190,12 +203,11 @@ public abstract class LivingEntityMixin extends Entity implements BlockWarpEntit
             this.mv$setIceBallCount(tag.getInt("marioverse:ice_ball_count"));
         }
 
-        this.mv$setFreezeImmunityCooldown(tag.getInt("marioverse:freeze_immunity_cooldown"));
-        this.mv$setFrozenCooldown(tag.getInt("marioverse:frozen_cooldown"));
-        this.mv$setOneUpsRewarded(tag.getInt("marioverse:one_ups_rewarded"));
-        this.mv$setPreventWarpCooldown(tag.getInt("marioverse:prevent_warp_cooldown"));
-        this.mv$setSuperStarCooldown(tag.getInt("marioverse:super_star_cooldown"));
-        this.mv$setWarpCooldown(tag.getInt("marioverse:warp_cooldown"));
+        if (entity.getType().is(TagRegistry.CAN_CONSUME_SUPER_STARS)
+                || ConfigRegistry.SUPER_STAR_POWERS_ALL_MOBS.get()) {
+            this.mv$setSuperStar(tag.getBoolean("marioverse:has_super_star"));
+            this.mv$setSuperStarCooldown(tag.getInt("marioverse:super_star_cooldown"));
+        }
     }
 
     @Inject( method = "tick", at = @At("TAIL"))
