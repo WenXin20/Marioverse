64bfa20a3edea81657fee9259e2343193f7902ca
WenXin2 <dwnewville@gmail.com>
2025-08-07 20:39:18 -0500

    Test changes

--- src/main/java/com/wenxin2/marioverse/integration/stone_zone_compat/MudModule.java
+++ src/main/java/com/wenxin2/marioverse/integration/stone_zone_compat/MudModule.java
@@ -8,7 +8,16 @@ import com.wenxin2.marioverse.blocks.StorageBrickBlock;
 import com.wenxin2.marioverse.blocks.entities.QuestionBlockEntity;
 import com.wenxin2.marioverse.registries.BlockRegistry;
 import com.wenxin2.marioverse.registries.TagRegistry;
+import java.io.IOException;
+import java.util.List;
+import net.mehvahdjukaar.every_compat.EveryCompat;
 import net.mehvahdjukaar.every_compat.api.SimpleEntrySet;
+import net.mehvahdjukaar.every_compat.dynamicpack.ClientDynamicResourcesHandler;
+import net.mehvahdjukaar.moonlight.api.resources.RPUtils;
+import net.mehvahdjukaar.moonlight.api.resources.textures.Palette;
+import net.mehvahdjukaar.moonlight.api.resources.textures.PaletteColor;
+import net.mehvahdjukaar.moonlight.api.resources.textures.Respriter;
+import net.mehvahdjukaar.moonlight.api.resources.textures.TextureImage;
 import net.mehvahdjukaar.moonlight.api.util.Utils;
 import net.mehvahdjukaar.stone_zone.api.StoneZoneEntrySet;
 import net.mehvahdjukaar.stone_zone.api.StoneZoneModule;
@@ -16,6 +25,8 @@ import net.mehvahdjukaar.stone_zone.api.set.MudType;
 import net.mehvahdjukaar.stone_zone.api.set.MudTypeRegistry;
 import net.minecraft.core.registries.Registries;
 import net.minecraft.resources.ResourceKey;
+import net.minecraft.resources.ResourceLocation;
+import net.minecraft.server.packs.resources.ResourceManager;
 import net.minecraft.tags.BlockTags;
 import net.minecraft.world.item.CreativeModeTab;
 import net.minecraft.world.level.block.Block;
@@ -118,5 +129,39 @@ public class MudModule extends StoneZoneModule {
                 .setTabKey(tab)
                 .build();
         this.addEntry(storageBricks);
+    }
+
+    @Override
+    public void addDynamicClientResources(ClientDynamicResourcesHandler handler, ResourceManager manager) {
+        super.addDynamicClientResources(handler, manager);
+
+        try (TextureImage questionOverlay = TextureImage.open(manager, EveryCompat.res("block/stone_question_bricks_overlay"));
+             TextureImage bricks = TextureImage.open(manager, ResourceLocation.parse("block/stone_bricks"))) {
+            storageBricks.blocks.forEach((mudType, block) -> {
+                try (TextureImage logSide_texture = TextureImage.open(manager, RPUtils.findFirstBlockTextureLocation(manager, mudType.mud));
+                     TextureImage logTop_texture = TextureImage.open(manager, RPUtils.findFirstBlockTextureLocation(manager, mudType.bricksOrStone()))) {
+                    ResourceLocation resLocITEM = EveryCompat.res("block/" + this.shortenedId() + "/" + mudType.getAppendableId() + "_question_bricks_overlay");
+                    Respriter respriterSIDE = Respriter.of(questionOverlay);
+                    Respriter respriterTOP = Respriter.of(bricks); // ITEM
+
+                    List<Palette> list_logSide = Palette.fromAnimatedImage(logSide_texture);
+                    List<Palette> list_logTop = Palette.fromAnimatedImage(logTop_texture);
+
+                    // Recoloring ITEM textures
+                    TextureImage recoloredITEM = respriterSIDE.recolor(list_logSide);
+                    TextureImage recoloredTOP = respriterTOP.recolor(list_logTop);
+                    recoloredITEM.applyOverlay(recoloredTOP);
+
+                    // Item Texture
+                    handler.dynamicPack.addAndCloseTexture(resLocITEM, recoloredITEM);
+
+                } catch (IOException e) {
+                    handler.getLogger().error("Failed to get Mud Brick Texture for {} : {}", block, e);
+                }
+            });
+        }
+        catch (IOException e) {
+            handler.getLogger().error("Failed to get Storage Brick Item Texture for ", e);
+        }
     }
 }
--- src/main/java/com/wenxin2/marioverse/integration/stone_zone_compat/StoneModule.java
+++ src/main/java/com/wenxin2/marioverse/integration/stone_zone_compat/StoneModule.java
@@ -8,8 +8,17 @@ import com.wenxin2.marioverse.blocks.StorageBrickBlock;
 import com.wenxin2.marioverse.blocks.entities.QuestionBlockEntity;
 import com.wenxin2.marioverse.registries.BlockRegistry;
 import com.wenxin2.marioverse.registries.TagRegistry;
+import java.io.IOException;
+import java.util.List;
+import net.mehvahdjukaar.every_compat.EveryCompat;
 import net.mehvahdjukaar.every_compat.api.RenderLayer;
 import net.mehvahdjukaar.every_compat.api.SimpleEntrySet;
+import net.mehvahdjukaar.every_compat.dynamicpack.ClientDynamicResourcesHandler;
+import net.mehvahdjukaar.every_compat.misc.SpriteHelper;
+import net.mehvahdjukaar.moonlight.api.resources.RPUtils;
+import net.mehvahdjukaar.moonlight.api.resources.textures.Palette;
+import net.mehvahdjukaar.moonlight.api.resources.textures.Respriter;
+import net.mehvahdjukaar.moonlight.api.resources.textures.TextureImage;
 import net.mehvahdjukaar.moonlight.api.util.Utils;
 import net.mehvahdjukaar.stone_zone.api.StoneZoneEntrySet;
 import net.mehvahdjukaar.stone_zone.api.StoneZoneModule;
@@ -17,6 +26,8 @@ import net.mehvahdjukaar.stone_zone.api.set.StoneType;
 import net.mehvahdjukaar.stone_zone.api.set.StoneTypeRegistry;
 import net.minecraft.core.registries.Registries;
 import net.minecraft.resources.ResourceKey;
+import net.minecraft.resources.ResourceLocation;
+import net.minecraft.server.packs.resources.ResourceManager;
 import net.minecraft.tags.BlockTags;
 import net.minecraft.world.item.CreativeModeTab;
 import net.minecraft.world.level.block.Block;
@@ -108,7 +119,7 @@ public class StoneModule extends StoneZoneModule {
                         BlockRegistry.STORAGE_STONE_BRICKS, StoneTypeRegistry::getStoneType,
                         stoneType -> new StorageBrickBlock(Utils.copyPropertySafe(stoneType.stone)))
                 .createPaletteFromBricks()
-                .addTexture(modRes("block/stone_question_bricks_overlay"))
+//                .addTexture(modRes("block/stone_question_bricks_overlay"))
                 .addTag(TagRegistry.COPYCAT_ALLOW, Registries.BLOCK)
                 .addTag(TagRegistry.SIMPLE_MOUNTED_STORAGE, Registries.BLOCK)
                 .addTag(BlockTags.MINEABLE_WITH_PICKAXE, Registries.BLOCK)
@@ -124,5 +135,39 @@ public class StoneModule extends StoneZoneModule {
                 .setTabKey(tab)
                 .build();
         this.addEntry(storageBricks);
+    }
+
+    @Override
+    public void addDynamicClientResources(ClientDynamicResourcesHandler handler, ResourceManager manager) {
+        super.addDynamicClientResources(handler, manager);
+
+        try (TextureImage questionOverlay = TextureImage.open(manager, EveryCompat.res("block/stone_question_bricks_overlay"));
+             TextureImage bricks = TextureImage.open(manager, ResourceLocation.parse("block/stone_bricks"))) {
+            storageBricks.blocks.forEach((stoneType, block) -> {
+                try (TextureImage logSide_texture = TextureImage.open(manager, RPUtils.findFirstBlockTextureLocation(manager, stoneType.stone));
+                     TextureImage logTop_texture = TextureImage.open(manager, RPUtils.findFirstBlockTextureLocation(manager, stoneType.bricksOrStone()))) {
+                    ResourceLocation resLocITEM = EveryCompat.res("block/" + this.shortenedId() + "/" + stoneType.getAppendableId() + "_question_bricks_overlay");
+                    Respriter respriterSIDE = Respriter.of(questionOverlay);
+                    Respriter respriterTOP = Respriter.of(bricks); // ITEM
+
+                    List<Palette> list_logSide = Palette.fromAnimatedImage(logSide_texture);
+                    List<Palette> list_logTop = Palette.fromAnimatedImage(logTop_texture);
+
+                    // Recoloring ITEM textures
+                    TextureImage recoloredITEM = respriterSIDE.recolor(list_logSide);
+                    TextureImage recoloredTOP = respriterTOP.recolor(list_logTop);
+                    recoloredITEM.applyOverlay(recoloredTOP);
+
+                    // Item Texture
+                    handler.dynamicPack.addAndCloseTexture(resLocITEM, recoloredITEM);
+
+                } catch (IOException e) {
+                    handler.getLogger().error("Failed to get Stone Brick Texture for {} : {}", block, e);
+                }
+            });
+        }
+        catch (IOException e) {
+            handler.getLogger().error("Failed to get Storage Brick Item Texture for ", e);
+        }
     }
 }
