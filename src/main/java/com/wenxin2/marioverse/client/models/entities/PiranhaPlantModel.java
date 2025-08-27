190b5e24721530200ec2bc8daead7ff32f13a42a
WenXin2 <dwnewville@gmail.com>
2025-08-26 22:31:49 -0500

    Removed piranha head turning

--- src/main/java/com/wenxin2/marioverse/client/models/entities/PiranhaPlantModel.java
+++ src/main/java/com/wenxin2/marioverse/client/models/entities/PiranhaPlantModel.java
@@ -1,15 +1,10 @@
 package com.wenxin2.marioverse.client.models.entities;
 
 import com.wenxin2.marioverse.Marioverse;
-import com.wenxin2.marioverse.entities.KoopaTroopaEntity;
 import com.wenxin2.marioverse.entities.PiranhaPlantEntity;
 import net.minecraft.client.renderer.RenderType;
 import net.minecraft.resources.ResourceLocation;
-import software.bernie.geckolib.animation.AnimationState;
-import software.bernie.geckolib.cache.object.GeoBone;
-import software.bernie.geckolib.constant.DataTickets;
 import software.bernie.geckolib.model.GeoModel;
-import software.bernie.geckolib.model.data.EntityModelData;
 
 public class PiranhaPlantModel extends GeoModel<PiranhaPlantEntity> {
     public PiranhaPlantModel() {
@@ -34,15 +29,5 @@ public class PiranhaPlantModel extends GeoModel<PiranhaPlantEntity> {
     @Override
     public ResourceLocation getAnimationResource(PiranhaPlantEntity animatable) {
         return ResourceLocation.fromNamespaceAndPath(Marioverse.MOD_ID, "animations/entity/piranha_plant/piranha_plant.animation.json");
-    }
-
-    @Override
-    public void setCustomAnimations(PiranhaPlantEntity animatable, long instanceId, AnimationState<PiranhaPlantEntity> animationState) {
-        GeoBone head = this.getAnimationProcessor().getBone("head_rotator");
-        if (head != null) {
-            EntityModelData entityData = animationState.getData(DataTickets.ENTITY_MODEL_DATA);
-            head.setRotX(entityData.headPitch() * 0.040F);
-            head.setRotY(entityData.netHeadYaw() * 0.025F);
-        }
     }
 }
