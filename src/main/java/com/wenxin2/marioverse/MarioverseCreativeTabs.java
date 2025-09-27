e89576c74025aa9605e9c2cb1f86d7bc095e38fb
WenXin2 <dwnewville@gmail.com>
2025-09-27 17:12:44 -0500

    Fix tab crash

--- src/main/java/com/wenxin2/marioverse/MarioverseCreativeTabs.java
+++ src/main/java/com/wenxin2/marioverse/MarioverseCreativeTabs.java
@@ -370,7 +370,7 @@ public class MarioverseCreativeTabs {
 
             add(event, Blocks.CALCITE);
             addDyedBlocks(event, Blocks.CALCITE, BlockRegistry.CALCITE, true, true);
-            addDyedBlocks(event, BlockRegistry.CALCITE_BUTTON, BlockRegistry.POLISHED_CALCITE, true, true);
+            addDyedBlocks(event, BlockRegistry.CALCITE.get(DyeColor.PINK), BlockRegistry.POLISHED_CALCITE, true, true);
             addDyedBlocks(event, BlockRegistry.POLISHED_CALCITE.get(DyeColor.PINK), BlockRegistry.CALCITE_BRICKS, true, true);
             addDyedBlocks(event, BlockRegistry.CALCITE_BRICKS.get(DyeColor.PINK), BlockRegistry.CRACKED_CALCITE_BRICKS, true, true);
             addDyedBlocks(event, BlockRegistry.CRACKED_CALCITE_BRICKS.get(DyeColor.PINK), BlockRegistry.CHISELED_CALCITE_BRICKS, true, true);
