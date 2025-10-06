b3d831e9ab3dfc0b94f5882a54c4f441a2316105
WenXin2 <dwnewville@gmail.com>
2025-10-05 23:02:15 -0500

    Add EC compat

--- src/main/java/com/wenxin2/marioverse/integration/wood_good_compat/WoodModule.java
+++ src/main/java/com/wenxin2/marioverse/integration/wood_good_compat/WoodModule.java
@@ -2,6 +2,7 @@ package com.wenxin2.marioverse.integration.wood_good_compat;
 
 import com.wenxin2.marioverse.MarioverseCreativeTabs;
 import com.wenxin2.marioverse.blocks.BridgeBlock;
+import com.wenxin2.marioverse.blocks.BridgeStairBlock;
 import com.wenxin2.marioverse.registries.BlockRegistry;
 import com.wenxin2.marioverse.registries.TagRegistry;
 import net.mehvahdjukaar.every_compat.EveryCompat;
@@ -13,6 +14,7 @@ import net.mehvahdjukaar.moonlight.api.util.Utils;
 import net.minecraft.core.registries.Registries;
 import net.minecraft.resources.ResourceKey;
 import net.minecraft.tags.BlockTags;
+import net.minecraft.tags.ItemTags;
 import net.minecraft.world.item.BlockItem;
 import net.minecraft.world.item.CreativeModeTab;
 import net.minecraft.world.level.block.Block;
@@ -20,6 +22,8 @@ import net.minecraft.world.level.block.Block;
 public class WoodModule extends SimpleModule {
     public final SimpleEntrySet<WoodType, Block> bridge;
     public final SimpleEntrySet<WoodType, Block> strippedBridge;
+    public final SimpleEntrySet<WoodType, Block> bridgeStairs;
+    public final SimpleEntrySet<WoodType, Block> strippedBridgeStairs;
 
     public WoodModule(String modId) {
         super(modId, "mv", EveryCompat.MOD_ID);
@@ -29,7 +33,9 @@ public class WoodModule extends SimpleModule {
                         BlockRegistry.OAK_LOG_BRIDGE, () -> VanillaWoodTypes.OAK,
                         woodType -> new BridgeBlock(woodType.log, Utils.copyPropertySafe(woodType.log)))
                 .addTag(BlockTags.MINEABLE_WITH_AXE, Registries.BLOCK)
+                .addTag(TagRegistry.FLAMMABLE_WOODEN_BRIDGE_BLOCKS, Registries.BLOCK)
                 .addTag(TagRegistry.WOODEN_BRIDGE_BLOCKS, Registries.BLOCK)
+                .addTag(TagRegistry.FLAMMABLE_WOODEN_BRIDGE_ITEMS, Registries.ITEM)
                 .addTag(TagRegistry.WOODEN_BRIDGE_ITEMS, Registries.ITEM)
                 .defaultRecipe()
                 .setTabKey(tab)
@@ -40,12 +46,45 @@ public class WoodModule extends SimpleModule {
                         BlockRegistry.STRIPPED_OAK_LOG_BRIDGE, () -> VanillaWoodTypes.OAK,
                         woodType -> new BridgeBlock(woodType.log, Utils.copyPropertySafe(woodType.log)))
                 .addTag(BlockTags.MINEABLE_WITH_AXE, Registries.BLOCK)
+                .addTag(TagRegistry.FLAMMABLE_WOODEN_BRIDGE_BLOCKS, Registries.BLOCK)
                 .addTag(TagRegistry.WOODEN_BRIDGE_BLOCKS, Registries.BLOCK)
+                .addTag(TagRegistry.FLAMMABLE_WOODEN_BRIDGE_ITEMS, Registries.ITEM)
                 .addTag(TagRegistry.WOODEN_BRIDGE_ITEMS, Registries.ITEM)
                 .requiresChildren("stripped_log")
                 .defaultRecipe()
                 .setTabKey(tab)
                 .build();
         this.addEntry(strippedBridge);
+
+        bridgeStairs = SimpleEntrySet.builder(WoodType.class, "log_bridge_stairs",
+                        BlockRegistry.OAK_LOG_BRIDGE_STAIRS, () -> VanillaWoodTypes.OAK,
+                        woodType -> new BridgeStairBlock(woodType.log.defaultBlockState(), Utils.copyPropertySafe(woodType.log)))
+                .addTag(BlockTags.MINEABLE_WITH_AXE, Registries.BLOCK)
+                .addTag(BlockTags.WOODEN_STAIRS, Registries.BLOCK)
+                .addTag(TagRegistry.FLAMMABLE_WOODEN_BRIDGE_STAIR_BLOCKS, Registries.BLOCK)
+                .addTag(TagRegistry.WOODEN_BRIDGE_STAIR_BLOCKS, Registries.BLOCK)
+                .addTag(TagRegistry.FLAMMABLE_WOODEN_BRIDGE_STAIR_ITEMS, Registries.ITEM)
+                .addTag(TagRegistry.WOODEN_BRIDGE_STAIR_ITEMS, Registries.ITEM)
+                .addTag(ItemTags.WOODEN_STAIRS, Registries.ITEM)
+                .defaultRecipe()
+                .setTabKey(tab)
+                .build();
+        this.addEntry(bridgeStairs);
+
+        strippedBridgeStairs = SimpleEntrySet.builder(WoodType.class, "log_bridge_stairs", "stripped",
+                        BlockRegistry.STRIPPED_OAK_LOG_BRIDGE_STAIRS, () -> VanillaWoodTypes.OAK,
+                        woodType -> new BridgeStairBlock(woodType.log.defaultBlockState(), Utils.copyPropertySafe(woodType.log)))
+                .addTag(BlockTags.MINEABLE_WITH_AXE, Registries.BLOCK)
+                .addTag(BlockTags.WOODEN_STAIRS, Registries.BLOCK)
+                .addTag(TagRegistry.FLAMMABLE_WOODEN_BRIDGE_STAIR_BLOCKS, Registries.BLOCK)
+                .addTag(TagRegistry.WOODEN_BRIDGE_STAIR_BLOCKS, Registries.BLOCK)
+                .addTag(TagRegistry.FLAMMABLE_WOODEN_BRIDGE_STAIR_ITEMS, Registries.ITEM)
+                .addTag(TagRegistry.WOODEN_BRIDGE_STAIR_ITEMS, Registries.ITEM)
+                .addTag(ItemTags.WOODEN_STAIRS, Registries.ITEM)
+                .requiresChildren("stripped_log")
+                .defaultRecipe()
+                .setTabKey(tab)
+                .build();
+        this.addEntry(strippedBridgeStairs);
     }
 }
