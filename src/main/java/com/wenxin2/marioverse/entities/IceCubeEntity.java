f1cdb0d2c43c22465b8bff03dacfaadb30db0739
WenXin2 <dwnewville@gmail.com>
2025-11-13 01:08:56 -0600

    Prevent turning of ice cube

--- src/main/java/com/wenxin2/marioverse/entities/IceCubeEntity.java
+++ src/main/java/com/wenxin2/marioverse/entities/IceCubeEntity.java
@@ -403,6 +403,7 @@ public class IceCubeEntity extends Mob implements GeoEntity, TraceableEntity {
                     this.position().add(width / 2, height, width / 2));
         }
         else return super.makeBoundingBox();
+    public void turn(double yaw, double pitch) {
     }
 
     @NotNull
@@ -411,15 +412,24 @@ public class IceCubeEntity extends Mob implements GeoEntity, TraceableEntity {
         if (this.getFrozenEntityData() != null) {
             float height = this.getFrozenEntityData().getFloat("Height") * 1.55F;
             float width = this.getFrozenEntityData().getFloat("Width") * 1.55F;
+    public void setYRot(float yaw) {
+    }
 
             return EntityDimensions.fixed(width, height);
         }
         return super.getDefaultDimensions(pose);
+    @Override
+    public void setYHeadRot(float yaw) {
     }
 
+    @Override
+    public void setYya(float yaw) {
+    }
 
     @Override
     public EntityDimensions getDimensions(Pose pose) {
+    public void setRot(float yaw, float pitch) {
+    }
         if (this.getFrozenEntityData() != null) {
             float height = this.getFrozenEntityData().getFloat("Height") * 1.55F;
             float width  = this.getFrozenEntityData().getFloat("Width")  * 1.55F;
