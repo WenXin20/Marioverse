830cc6147044fde2c4e6bf33c04e15ca5612673a
WenXin2 <dwnewville@gmail.com>
2025-12-16 22:41:07 -0600

    Add snowball rotation

--- src/main/java/com/wenxin2/marioverse/client/renderers/entities/projectile/LargeSnowballRenderer.java
+++ src/main/java/com/wenxin2/marioverse/client/renderers/entities/projectile/LargeSnowballRenderer.java
@@ -1,14 +1,16 @@
 package com.wenxin2.marioverse.client.renderers.entities.projectile;
 
 import com.mojang.blaze3d.vertex.PoseStack;
-import com.mojang.math.Axis;
+import com.mojang.blaze3d.vertex.VertexConsumer;
 import com.wenxin2.marioverse.client.models.entities.projectiles.LargeSnowballModel;
 import com.wenxin2.marioverse.entities.projectiles.LargeSnowballProjectile;
-import net.minecraft.client.renderer.LightTexture;
 import net.minecraft.client.renderer.MultiBufferSource;
 import net.minecraft.client.renderer.entity.EntityRendererProvider;
+import net.minecraft.util.Mth;
+import net.minecraft.world.phys.Vec3;
+import org.jetbrains.annotations.Nullable;
+import software.bernie.geckolib.cache.object.BakedGeoModel;
 import software.bernie.geckolib.renderer.GeoEntityRenderer;
-import software.bernie.geckolib.renderer.layer.AutoGlowingGeoLayer;
 
 public class LargeSnowballRenderer extends GeoEntityRenderer<LargeSnowballProjectile> {
     public LargeSnowballRenderer(EntityRendererProvider.Context renderManager) {
@@ -16,11 +18,24 @@ public class LargeSnowballRenderer extends GeoEntityRenderer<LargeSnowballProjec
     }
 
     @Override
-    public void render(LargeSnowballProjectile entity, float entityYaw, float partialTicks, PoseStack poseStack, MultiBufferSource buffer, int packedLight) {
-//        poseStack.pushPose();
-//            poseStack.mulPose(Axis.YP.rotationDegrees(-entity.getYRot()));
-//            poseStack.mulPose(Axis.XP.rotationDegrees(entity.getXRot()));
-            super.render(entity, entityYaw, partialTicks, poseStack, buffer, packedLight);
-//        poseStack.popPose();
+    public void preRender(PoseStack poseStack, LargeSnowballProjectile animatable, BakedGeoModel model, @Nullable MultiBufferSource bufferSource, @Nullable VertexConsumer buffer, boolean isReRender, float partialTick, int packedLight, int packedOverlay, int colour) {
+        this.model.getBone("root").ifPresent(root -> {
+            Vec3 velocity = animatable.getDeltaMovement();
+            float horizontalSpeed = (float) velocity.horizontalDistance();
+            horizontalSpeed = Mth.clamp(horizontalSpeed, 0.0F, 2.0F);
+
+            float time = animatable.tickCount + partialTick;
+            float roll = time * horizontalSpeed * 0.35F;
+
+            if (velocity.horizontalDistanceSqr() > 0.0001) {
+                float yawRad = (float) Math.atan2(velocity.x, velocity.z);
+                root.setRotY(yawRad);
+            }
+
+            root.setRotX(-roll);
+            root.setRotZ(0.0F);
+        });
+
+        super.preRender(poseStack, animatable, model, bufferSource, buffer, isReRender, partialTick, packedLight, packedOverlay, colour);
     }
 }
