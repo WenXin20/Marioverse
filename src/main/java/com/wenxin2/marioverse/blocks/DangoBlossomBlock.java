06bc7fe8ff1be779574546ddf47d2dc1517042ec
WenXin2 <dwnewville@gmail.com>
2025-12-30 12:38:33 -0600

    Fix dango blossom placement

--- src/main/java/com/wenxin2/marioverse/blocks/DangoBlossomBlock.java
+++ src/main/java/com/wenxin2/marioverse/blocks/DangoBlossomBlock.java
@@ -67,8 +67,8 @@ public class DangoBlossomBlock extends Block implements SimpleWaterloggedBlock {
     @Override
     protected boolean canSurvive(BlockState state, LevelReader worldReader, BlockPos pos) {
         return Block.canSupportCenter(worldReader, pos.below(), Direction.UP)
-                || worldReader.getBiome(pos.below()) instanceof CactusBlock
-                || worldReader.getBiome(pos.below()) instanceof LeavesBlock;
+                || worldReader.getBlockState(pos.below()).getBlock() instanceof CactusBlock
+                || worldReader.getBlockState(pos.below()).getBlock() instanceof LeavesBlock;
     }
 
     @Override
