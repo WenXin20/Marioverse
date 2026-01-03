13a44004c234a272ca7d057473d9437e8a895b9a
WenXin2 <dwnewville@gmail.com>
2026-01-02 22:58:45 -0600

    Remove unused

--- src/main/java/com/wenxin2/marioverse/entities/power_ups/MushroomEntity.java
+++ src/main/java/com/wenxin2/marioverse/entities/power_ups/MushroomEntity.java
@@ -2,15 +2,8 @@ package com.wenxin2.marioverse.entities.power_ups;
 
 import com.wenxin2.marioverse.entities.ai.goals.ContinuousStrollGoal;
 import com.wenxin2.marioverse.registries.ConfigRegistry;
-import com.wenxin2.marioverse.registries.ParticleRegistry;
-import com.wenxin2.marioverse.registries.SoundRegistry;
-import com.wenxin2.marioverse.registries.TagRegistry;
 import com.wenxin2.marioverse.utils.AbilitiesHandler;
-import com.wenxin2.marioverse.utils.ServerParticleUtils;
 import java.util.List;
-import javax.annotation.Nullable;
-import net.minecraft.server.level.ServerLevel;
-import net.minecraft.sounds.SoundSource;
 import net.minecraft.world.entity.Entity;
 import net.minecraft.world.entity.EntityType;
 import net.minecraft.world.entity.LivingEntity;
@@ -87,47 +80,5 @@ public class MushroomEntity extends BaseMushroomEntity implements GeoEntity {
     public void collideWithEntity(Entity entity) {
         if (entity instanceof LivingEntity livingEntity && entity instanceof AbilitiesHandler handler)
             handler.applyMushroomPowerUp(this.level(), livingEntity, this, ConfigRegistry.SUPER_MUSHROOM_HEALTH_HEALED.get().floatValue());
-    }
-
-    public static void powerUp(Level world, Entity entity, @Nullable Entity mushroom, float healthHealed) {
-        if (entity instanceof Player player && !player.isSpectator()
-                && !player.getType().is(TagRegistry.CANNOT_CONSUME_POWER_UPS)
-                && (player.getType().is(TagRegistry.CAN_CONSUME_SUPER_MUSHROOMS)
-                    || ConfigRegistry.SUPER_MUSHROOM_POWERS_ALL_MOBS.get())
-                && player instanceof AbilitiesHandler handler) {
-            handler.mv$setSuperMushroom(true);
-            if (entity.level() instanceof ServerLevel serverWorld)
-                ServerParticleUtils.spawnPoweredUpParticles(ParticleRegistry.POWERED_UP.get(), serverWorld, entity, 25);
-
-            if (!world.isClientSide) {
-                if (player.getHealth() < player.getMaxHealth())
-                    player.heal(healthHealed);
-                if (mushroom != null) {
-                    world.playSound(null, mushroom.blockPosition(), SoundRegistry.POWERS_UP.get(),
-                            SoundSource.PLAYERS, 1.0F, 1.0F);
-                    mushroom.remove(RemovalReason.KILLED);
-                } else world.playSound(null, entity.blockPosition(), SoundRegistry.POWERS_UP.get(),
-                        SoundSource.PLAYERS, 1.0F, 1.0F);
-            }
-        } else if (entity instanceof LivingEntity livingEntity
-                && !livingEntity.getType().is(TagRegistry.CANNOT_CONSUME_POWER_UPS)
-                && (livingEntity.getType().is(TagRegistry.CAN_CONSUME_SUPER_MUSHROOMS)
-                    || ConfigRegistry.SUPER_MUSHROOM_POWERS_ALL_MOBS.get())
-                && entity instanceof AbilitiesHandler handler) {
-            handler.mv$setSuperMushroom(true);
-            if (entity.level() instanceof ServerLevel serverWorld)
-                ServerParticleUtils.spawnPoweredUpParticles(ParticleRegistry.POWERED_UP.get(), serverWorld, entity, 25);
-
-            if (!world.isClientSide) {
-                if (livingEntity.getHealth() < livingEntity.getMaxHealth())
-                    livingEntity.heal(healthHealed);
-                if (mushroom != null) {
-                    world.playSound(null, mushroom.blockPosition(), SoundRegistry.POWERS_UP.get(),
-                            SoundSource.NEUTRAL, 1.0F, 1.0F);
-                    mushroom.remove(RemovalReason.KILLED);
-                } else world.playSound(null, entity.blockPosition(), SoundRegistry.POWERS_UP.get(),
-                        SoundSource.NEUTRAL, 1.0F, 1.0F);
-            }
-        }
     }
 }
