513043fd7c99a312142488ae6aa94238884cc3db
WenXin2 <dwnewville@gmail.com>
2026-01-18 19:04:56 -0600

    Remove old warp fuel

--- src/main/java/com/wenxin2/marioverse/registries/ConfigRegistry.java
+++ src/main/java/com/wenxin2/marioverse/registries/ConfigRegistry.java
@@ -214,12 +214,10 @@ public class ConfigRegistry {
     public static ModConfigSpec.IntValue SUPER_STAR_SPEED_DURATION;
     public static ModConfigSpec.IntValue WARP_DISRUPTING_COOLDOWN;
     public static ModConfigSpec.IntValue WARP_DOOR_COOLDOWN;
-    public static ModConfigSpec.IntValue WARP_DOOR_FUEL_AMT;
     public static ModConfigSpec.IntValue WARP_PAINTING_COOLDOWN;
     public static ModConfigSpec.IntValue WARP_PAINTING_FUEL_AMT;
     public static ModConfigSpec.IntValue WARP_PIPE_COOLDOWN;
     public static ModConfigSpec.IntValue WARP_TRAPDOOR_COOLDOWN;
-    public static ModConfigSpec.IntValue WARP_TRAPDOOR_FUEL_AMT;
 
     private ConfigRegistry() {
         ModConfigSpec.Builder BUILDER = new ModConfigSpec.Builder();
@@ -406,9 +404,6 @@ public class ConfigRegistry {
                             .comment("Cooldown between teleports in ticks.")
                             .comment("§6[20 ticks = 1 second]§b")
                             .defineInRange("warp_door_cooldown", 70, 0, 72000);
-                    WARP_DOOR_FUEL_AMT = BUILDER.translation("configuration.marioverse.warp_door_fuel_amt")
-                            .comment("Amount of fuel required to create a warp door.§b")
-                            .defineInRange("warp_door_fuel_amt", 2, 0, 64);
                 BUILDER.pop();
 
                 BUILDER.push(CATEGORY_WARP_PAINTINGS);
@@ -480,9 +475,6 @@ public class ConfigRegistry {
                             .comment("§6[20 ticks = 1 second]")
                             .comment("§9[Default: 70]§b")
                             .defineInRange("warp_trapdoor_cooldown", 70, 0, 72000);
-                    WARP_TRAPDOOR_FUEL_AMT = BUILDER.translation("configuration.marioverse.warp_trapdoor_fuel_amt")
-                            .comment("Amount of fuel required to create a warp trapdoor.§b")
-                            .defineInRange("warp_trapdoor_fuel_amt", 2, 0, 64);
                 BUILDER.pop();
 
                 BUILDER.push(CATEGORY_WATER_SPOUTS);
