47543783fbd25d459d830864e796a8430a9a8fdc
WenXin2 <dwnewville@gmail.com>
2026-01-19 23:22:26 -0600

    Rename classes

--- src/main/java/com/wenxin2/marioverse/client/models/entities/SuperMushroomModel.java
+++ src/main/java/com/wenxin2/marioverse/client/models/entities/SuperMushroomModel.java
@@ -2,17 +2,18 @@ package com.wenxin2.marioverse.client.models.entities;
 
 import com.wenxin2.marioverse.Marioverse;
 import com.wenxin2.marioverse.entities.power_ups.MushroomEntity;
+import com.wenxin2.marioverse.entities.power_ups.SuperMushroomEntity;
 import net.minecraft.client.renderer.RenderType;
 import net.minecraft.resources.ResourceLocation;
 import software.bernie.geckolib.model.DefaultedEntityGeoModel;
 
-public class MushroomModel extends DefaultedEntityGeoModel<MushroomEntity> {
-    public MushroomModel() {
+public class SuperMushroomModel extends DefaultedEntityGeoModel<SuperMushroomEntity> {
+    public SuperMushroomModel() {
         super(ResourceLocation.fromNamespaceAndPath(Marioverse.MOD_ID, "power_up/super_mushroom"));
     }
 
     @Override
-    public RenderType getRenderType(MushroomEntity animatable, ResourceLocation texture) {
+    public RenderType getRenderType(SuperMushroomEntity animatable, ResourceLocation texture) {
         return RenderType.entitySolid(getTextureResource(animatable));
     }
 }
