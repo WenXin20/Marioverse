2c8eb541c3886f93f13043d395f1aab48c4f9bdd
WenXin2 <dwnewville@gmail.com>
2026-01-25 16:13:22 -0600

    Remove max health after mega ends

--- src/main/java/com/wenxin2/marioverse/mixin/LivingEntityMixin.java
+++ src/main/java/com/wenxin2/marioverse/mixin/LivingEntityMixin.java
@@ -311,8 +311,16 @@ public abstract class LivingEntityMixin extends Entity implements BlockWarpEntit
 
         if (entity.getData(DataAttachmentRegistry.MEGA_MUSHROOM_DURATION) == 0
                 && entity.getData(DataAttachmentRegistry.HAS_MEGA_MUSHROOM)) {
+            AttributeInstance attribute = entity.getAttribute(Attributes.MAX_HEALTH);
+
             entity.setData(DataAttachmentRegistry.HAS_MEGA_MUSHROOM, false);
 //            entity.setData(DataAttachmentRegistry.PLAYED_SUPER_STAR_THEME, false); // TODO
+
+            if (attribute != null && !entity.getData(DataAttachmentRegistry.HAS_MINI_MUSHROOM)) {
+                AttributeModifier modifier = attribute.getModifier(AttributesRegistry.MAX_HEATH);
+                if (modifier != null)
+                    attribute.removeModifier(modifier);
+            }
         }
 
         if (entity.getData(DataAttachmentRegistry.SUPER_STAR_COOLDOWN) > 0)
