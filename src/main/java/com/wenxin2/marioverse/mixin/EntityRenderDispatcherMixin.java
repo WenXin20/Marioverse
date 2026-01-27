5e050cc22f5f70c754043dc3f342a658c2fe23bc
WenXin2 <dwnewville@gmail.com>
2026-01-27 01:48:14 -0600

    Fix hitbox render scale

--- src/main/java/com/wenxin2/marioverse/mixin/EntityRenderDispatcherMixin.java
+++ src/main/java/com/wenxin2/marioverse/mixin/EntityRenderDispatcherMixin.java
@@ -9,29 +9,30 @@ import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.phys.AABB;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.ModifyArgs;
 import org.spongepowered.asm.mixin.injection.ModifyVariable;
+import org.spongepowered.asm.mixin.injection.invoke.arg.Args;
 
 @Mixin(EntityRenderDispatcher.class)
 public class EntityRenderDispatcherMixin {
     @ModifyVariable(method = "renderHitbox", at = @At("STORE"), ordinal = 0)
     private static AABB renderHitbox(AABB aabb, PoseStack poseStack, VertexConsumer vertexConsumer, Entity entity,
-                                           float tickDelta, float red, float green, float blue) {
+                                     float tickDelta, float red, float green, float blue) {
         if (!(entity instanceof LivingEntity livingEntity))
             return aabb;
+
         double heightScale = livingEntity.getAttributes().getValue(AttributesRegistry.HEIGHT_SCALE);
         double widthScale  = livingEntity.getAttributes().getValue(AttributesRegistry.WIDTH_SCALE);
 
-        if (widthScale < 1.0 || heightScale < 1.0) {
-            double width = (1.0 / widthScale) - 1.0;
-            double height = (1.0 / heightScale) - 1.0;
+        double width = (1.0 / widthScale) - 1.0;
+        double height = (1.0 / heightScale) - 1.0;
 
-            return new AABB(aabb.minX - aabb.getXsize() * width / 2,
-                    aabb.minY,
-                    aabb.minZ - aabb.getZsize() * width / 2,
-                    aabb.maxX + aabb.getXsize() * width / 2,
-                    aabb.maxY + aabb.getYsize() * height,
-                    aabb.maxZ + aabb.getZsize() * width / 2);
-        }
-        return aabb;
+        return new AABB(aabb.minX - aabb.getXsize() * width / 2,
+                aabb.minY,
+                aabb.minZ - aabb.getZsize() * width / 2,
+                aabb.maxX + aabb.getXsize() * width / 2,
+                aabb.maxY + aabb.getYsize() * height,
+                aabb.maxZ + aabb.getZsize() * width / 2);
+    }
     }
 }
