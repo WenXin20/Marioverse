ec2c1acb2b3db0069f3bf720100b97f070b3ee63
WenXin2 <dwnewville@gmail.com>
2026-03-26 00:42:42 -0500

    Add dango blossom to shaders

--- src/main/java/com/wenxin2/marioverse/mixin/iris/IdMapMixin.java
+++ src/main/java/com/wenxin2/marioverse/mixin/iris/IdMapMixin.java
@@ -18,9 +18,10 @@ public class IdMapMixin {
     @WrapOperation(method = "lambda$parseBlockMap$2", at = @At(value = "INVOKE", target = "Ljava/util/List;add(Ljava/lang/Object;)Z"), remap = false)
     private static boolean addBlockEntry(List instance, Object entry, Operation<Boolean> original) {
         if (entry instanceof BlockEntry blockEntry) {
-            if (blockEntry.id().getNamespace().equals("minecraft") && blockEntry.id().getName().equals("water")) {
+            if (blockEntry.id().getNamespace().equals("minecraft") && blockEntry.id().getName().equals("water"))
                 original.call(instance, new BlockEntry(new NamespacedId(Marioverse.MOD_ID, BlockRegistry.WATER_SPOUT.getId().getPath()), Collections.emptyMap()));
-            }
+            if (blockEntry.id().getNamespace().equals("minecraft") && blockEntry.id().getName().equals("dandelion"))
+                original.call(instance, new BlockEntry(new NamespacedId(Marioverse.MOD_ID, BlockRegistry.DANGO_BLOSSOM.getId().getPath()), Collections.emptyMap()));
         }
         return original.call(instance, entry);
     }
