0534106864f5a0f6fb19848a88f0f40e56f46670
WenXin2 <dwnewville@gmail.com>
2026-05-16 22:22:32 -0500

    Food fix

--- src/main/java/com/wenxin2/marioverse/registries/FoodRegistry.java
+++ src/main/java/com/wenxin2/marioverse/registries/FoodRegistry.java
@@ -4,9 +4,9 @@ import net.minecraft.world.food.FoodProperties;
 
 public class FoodRegistry {
     public static final FoodProperties COOKED_CHEEP_CHEEP = new FoodProperties.Builder()
-            .fast().alwaysEdible().nutrition(6).saturationModifier(0.8F).build();
+            .nutrition(6).saturationModifier(0.8F).build();
     public static final FoodProperties DASH_MUSHROOM = new FoodProperties.Builder()
             .fast().alwaysEdible().nutrition(4).saturationModifier(0.3F).build();
     public static final FoodProperties RAW_CHEEP_CHEEP = new FoodProperties.Builder()
-            .fast().alwaysEdible().nutrition(2).saturationModifier(0.1F).build();
+            .nutrition(2).saturationModifier(0.1F).build();
 }
