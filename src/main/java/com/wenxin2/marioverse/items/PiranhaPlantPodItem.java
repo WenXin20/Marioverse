807258800ac8e96343089c74ff8a62988d7eed0c
WenXin2 <dwnewville@gmail.com>
2026-06-12 02:02:06 -0500

    Fix pod

--- src/main/java/com/wenxin2/marioverse/items/PiranhaPlantPodItem.java
+++ src/main/java/com/wenxin2/marioverse/items/PiranhaPlantPodItem.java
@@ -32,16 +32,16 @@ public class PiranhaPlantPodItem extends BetterSpawnEggItem {
     @Override
     public InteractionResult useOn(UseOnContext context) {
         Level world = context.getLevel();
-        if (!(world instanceof ServerLevel)) {
+        if (!(world instanceof ServerLevel))
             return InteractionResult.SUCCESS;
-        } else {
+        else {
             ItemStack stack = context.getItemInHand();
             BlockPos pos = context.getClickedPos();
             Direction direction = context.getClickedFace();
             BlockState state = world.getBlockState(pos);
 
             if (world.getBlockEntity(pos) instanceof Spawner spawner
-                    && (context.getPlayer() != null && !context.getPlayer().isShiftKeyDown())) {
+                    && (context.getPlayer() != null && context.getPlayer().isCreative() && !context.getPlayer().isShiftKeyDown())) {
                 EntityType<?> entityType1 = this.getType(stack);
                 spawner.setEntityId(entityType1, world.getRandom());
                 world.sendBlockUpdated(pos, state, state, 3);
@@ -57,24 +57,24 @@ public class PiranhaPlantPodItem extends BetterSpawnEggItem {
                 stack.shrink(1);
                 return InteractionResult.CONSUME;
             } else {
-
                 EntityType<?> entityType = this.getType(stack);
-                BlockPos spawnPos = pos.relative(context.getClickedFace());
+                Entity entity = entityType.create(world);
 
-                Entity entity = entityType.spawn((ServerLevel) world, stack, context.getPlayer(), spawnPos, MobSpawnType.SPAWN_EGG, true,
-                        direction == Direction.UP);
-                if (entity != null) {
+                if (entity instanceof PiranhaPlantEntity piranhaPlant) {
+                    piranhaPlant.setAge(-24000);
+                    BlockPos spawnPos = pos.relative(context.getClickedFace());
+
+                    piranhaPlant.moveTo(spawnPos.getX() + 0.5D, spawnPos.getY(), spawnPos.getZ() + 0.5D,
+                            piranhaPlant.getYRot(), piranhaPlant.getXRot());
+
+                    BlockPos newPos = piranhaPlant.findValidBlockPos();
+
+                    piranhaPlant.attachToBlock(newPos, context.getClickedFace().getOpposite());
+                    piranhaPlant.setOwner(context.getPlayer());
+                    world.addFreshEntity(piranhaPlant);
                     stack.shrink(1);
                     world.gameEvent(context.getPlayer(), GameEvent.ENTITY_PLACE, pos);
-
-                    if (entity instanceof PiranhaPlantEntity piranhaPlant) {
-                        piranhaPlant.setAge(-24000);
-                        BlockPos newPos = piranhaPlant.findValidBlockPos();
-                        piranhaPlant.attachToBlock(newPos, context.getClickedFace().getOpposite());
-                        piranhaPlant.setOwner(context.getPlayer());
-                    }
                 }
-
                 return InteractionResult.CONSUME;
             }
         }
