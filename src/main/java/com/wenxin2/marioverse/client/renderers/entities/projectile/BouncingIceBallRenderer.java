b7108559366263c2bf8b44d9a9c29b65f1c27118
WenXin2 <dwnewville@gmail.com>
2026-08-08 01:29:12 -0500

    Revert "Attempt fix"
    This reverts commit a3d8c10719c1eab58d4dfd78272d8a907e2adb1d.

--- src/main/java/com/wenxin2/marioverse/client/renderers/entities/projectile/BouncingIceBallRenderer.java
+++ src/main/java/com/wenxin2/marioverse/client/renderers/entities/projectile/BouncingIceBallRenderer.java
@@ -16,15 +16,18 @@ import software.bernie.geckolib.renderer.layer.AutoGlowingGeoLayer;
 public class BouncingIceBallRenderer extends GeoEntityRenderer<BouncingIceBallProjectile> {
     public BouncingIceBallRenderer(EntityRendererProvider.Context renderManager) {
         super(renderManager, new BouncingIceBallModel());
-        this.addRenderLayer(new AutoGlowingGeoLayer<>(this));
+        addRenderLayer(new AutoGlowingGeoLayer<>(this));
     }
 
     @Override
     public void preRender(PoseStack poseStack, BouncingIceBallProjectile animatable, BakedGeoModel model,
                           @Nullable MultiBufferSource bufferSource, @Nullable VertexConsumer buffer,
                           boolean isReRender, float partialTick, int packedLight, int packedOverlay, int colour) {
-        poseStack.mulPose(Axis.YP.rotationDegrees(45)); // hardcoded, ignore entity rotation entirely
-        super.preRender(poseStack, animatable, model, bufferSource, buffer, isReRender, partialTick,
-                LightTexture.FULL_BRIGHT, packedOverlay, colour);
+        poseStack.pushPose();
+            poseStack.mulPose(Axis.YP.rotationDegrees(-animatable.getYRot()));
+            poseStack.mulPose(Axis.XP.rotationDegrees(animatable.getXRot()));
+            super.preRender(poseStack, animatable, model, bufferSource, buffer, isReRender, partialTick,
+                    LightTexture.FULL_BRIGHT, packedOverlay, colour);
+        poseStack.popPose();
     }
 }
