2573746935ae2a43d8026a0c73a73c2648333e9b
WenXin2 <dwnewville@gmail.com>
2026-08-12 01:28:56 -0500

    Every compat support
    Update WoodModule.java

--- src/main/java/com/wenxin2/marioverse/integration/wood_good_compat/WoodModule.java
+++ src/main/java/com/wenxin2/marioverse/integration/wood_good_compat/WoodModule.java
@@ -3,27 +3,32 @@ package com.wenxin2.marioverse.integration.wood_good_compat;
 import com.wenxin2.marioverse.MarioverseCreativeTabs;
 import com.wenxin2.marioverse.blocks.BridgeBlock;
 import com.wenxin2.marioverse.blocks.BridgeStairBlock;
+import com.wenxin2.marioverse.blocks.PicketFenceBlock;
 import com.wenxin2.marioverse.registries.BlockRegistry;
 import com.wenxin2.marioverse.registries.TagRegistry;
 import net.mehvahdjukaar.every_compat.EveryCompat;
+import net.mehvahdjukaar.every_compat.api.PaletteStrategies;
 import net.mehvahdjukaar.every_compat.api.SimpleEntrySet;
 import net.mehvahdjukaar.every_compat.api.SimpleModule;
 import net.mehvahdjukaar.every_compat.modules.EveryCompatModule;
 import net.mehvahdjukaar.moonlight.api.set.wood.VanillaWoodTypes;
 import net.mehvahdjukaar.moonlight.api.set.wood.WoodType;
 import net.mehvahdjukaar.moonlight.api.util.Utils;
+import net.mehvahdjukaar.stone_zone.api.StonePaletteStrategies;
 import net.minecraft.core.registries.Registries;
 import net.minecraft.resources.ResourceKey;
 import net.minecraft.tags.BlockTags;
 import net.minecraft.tags.ItemTags;
 import net.minecraft.world.item.CreativeModeTab;
 import net.minecraft.world.level.block.Block;
+import net.neoforged.neoforge.common.Tags;
 import net.neoforged.neoforge.registries.DeferredHolder;
 
 public class WoodModule extends EveryCompatModule {
     public final SimpleEntrySet<WoodType, Block> bridge;
-    public final SimpleEntrySet<WoodType, Block> strippedBridge;
     public final SimpleEntrySet<WoodType, Block> bridgeStairs;
+    public final SimpleEntrySet<WoodType, Block> picketFence;
+    public final SimpleEntrySet<WoodType, Block> strippedBridge;
     public final SimpleEntrySet<WoodType, Block> strippedBridgeStairs;
 
     public WoodModule(String modId) {
@@ -87,5 +92,24 @@ public class WoodModule extends EveryCompatModule {
                 .setTab(buildingBlocksTab)
                 .build();
         this.addEntry(strippedBridgeStairs);
+
+        picketFence = SimpleEntrySet.builder(WoodType.class, "picket_fence",
+                        BlockRegistry.OAK_PICKET_FENCE, () -> VanillaWoodTypes.OAK,
+                        woodType -> new PicketFenceBlock(Utils.copyPropertySafe(woodType.planks)))
+                .addTexture(modRes("block/oak_picket_fence"), PaletteStrategies.PLANKS_STANDARD)
+                .addTextureM(modRes("block/oak_picket_fence_back"), EveryCompat.res("block/mv/oak_picket_fence_back_mask"),
+                        PaletteStrategies.PLANKS_STANDARD)
+                .addTag(BlockTags.MINEABLE_WITH_AXE, Registries.BLOCK)
+                .addTag(TagRegistry.FLAMMABLE_WOODEN_PICKET_FENCES, Registries.BLOCK)
+                .addTag(TagRegistry.WOODEN_PICKET_FENCES, Registries.BLOCK)
+                .addTag(TagRegistry.FLAMMABLE_WOODEN_PICKET_FENCE_ITEMS, Registries.ITEM)
+                .addTag(TagRegistry.WOODEN_PICKET_FENCE_ITEMS, Registries.ITEM)
+                .addTag(BlockTags.WOODEN_FENCES, Registries.ITEM)
+                .addTag(Tags.Blocks.FENCES_WOODEN, Registries.ITEM)
+                .requiresChildren("planks")
+                .setTab(buildingBlocksTab)
+                .defaultRecipe()
+                .build();
+        this.addEntry(picketFence);
     }
 }
