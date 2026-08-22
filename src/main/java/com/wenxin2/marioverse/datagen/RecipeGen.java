4ec854f9c32b9327fbfe36d93f7fa232fefd2c0b
WenXin2 <dwnewville@gmail.com>
2026-08-22 18:26:03 -0500

    Fix recipe

--- src/main/java/com/wenxin2/marioverse/datagen/RecipeGen.java
+++ src/main/java/com/wenxin2/marioverse/datagen/RecipeGen.java
@@ -98,7 +98,7 @@ public class RecipeGen extends RecipeUtils {
         abilityBlockRecipe(1, "ability_blocks", BlockRegistry.STEVE_ABILITY_BLOCK, RecipeCategory.COMBAT, ItemTags.TRIM_TEMPLATES, Blocks.CYAN_WOOL, Tags.Items.GEMS_LAPIS, Blocks.CYAN_WOOL, false, output);
         abilityBlockRecipe(1, "ability_blocks", BlockRegistry.WALUIGI_ABILITY_BLOCK, RecipeCategory.COMBAT, ItemRegistry.WALUIGI_ARMOR_TRIM_SMITHING_TEMPLATE, Tags.Items.SLIME_BALLS, Tags.Items.GEMS_DIAMOND, ItemTags.WOOL, false, output);
         abilityBlockRecipe(1, "ability_blocks", BlockRegistry.WARIO_ABILITY_BLOCK, RecipeCategory.COMBAT, ItemRegistry.WARIO_ARMOR_TRIM_SMITHING_TEMPLATE, Tags.Items.SLIME_BALLS, Tags.Items.GEMS_DIAMOND, ItemTags.WOOL, false, output);
-        arrowSignRecipe(5, "arrow_signs", ItemRegistry.MUSHROOT_ARROW_SIGN, RecipeCategory.DECORATIONS, TagRegistry.MUSHROOT_PLANK_ITEMS, Tags.Items.CHAINS, Tags.Items.DYES_RED, false, output);
+        arrowSignRecipe(5, "arrow_signs", ItemRegistry.MUSHROOT_ARROW_SIGN, RecipeCategory.DECORATIONS, TagRegistry.MUSHROOT_PLANK_ITEMS, Tags.Items.DYES_RED, Tags.Items.CHAINS, false, output);
         boatRecipe(1, "boats", ItemRegistry.MUSHROOT_BOAT, RecipeCategory.TRANSPORTATION, TagRegistry.MUSHROOT_PLANK_ITEMS, false, output);
         chestBoatRecipe(1, "chest_boats", ItemRegistry.MUSHROOT_CHEST_BOAT, RecipeCategory.TRANSPORTATION, Tags.Items.CHESTS_WOODEN, ItemRegistry.MUSHROOT_BOAT, false, output);
         checkeredRecipe(4, "spike_panels", BlockRegistry.CALCITE_CHECKERED_TILES, BlockRegistry.POLISHED_CALCITE.get(DyeColor.WHITE), BlockRegistry.POLISHED_CALCITE.get(DyeColor.BLACK), false, output);
