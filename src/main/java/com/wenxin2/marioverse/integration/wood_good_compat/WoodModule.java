f0f7d0bc3992d6493efdb103195c66c8a918b8d4
WenXin2 <dwnewville@gmail.com>
2026-08-30 01:22:23 -0500

    Fix item particles

--- src/main/java/com/wenxin2/marioverse/integration/wood_good_compat/WoodModule.java
+++ src/main/java/com/wenxin2/marioverse/integration/wood_good_compat/WoodModule.java
@@ -1,6 +1,5 @@
 package com.wenxin2.marioverse.integration.wood_good_compat;
 
-import com.wenxin2.marioverse.Marioverse;
 import com.wenxin2.marioverse.MarioverseCreativeTabs;
 import com.wenxin2.marioverse.blocks.BridgeBlock;
 import com.wenxin2.marioverse.blocks.BridgeStairBlock;
@@ -15,22 +14,14 @@ import com.wenxin2.marioverse.items.LargeArrowSignItem;
 import com.wenxin2.marioverse.registries.BlockEntityRegistry;
 import com.wenxin2.marioverse.registries.BlockRegistry;
 import com.wenxin2.marioverse.registries.TagRegistry;
-import com.google.gson.JsonObject;
-import java.util.Map;
-import java.util.function.Consumer;
 import net.mehvahdjukaar.every_compat.EveryCompat;
 import net.mehvahdjukaar.every_compat.api.PaletteStrategies;
 import net.mehvahdjukaar.every_compat.api.SimpleEntrySet;
 import net.mehvahdjukaar.every_compat.modules.EveryCompatModule;
-import net.mehvahdjukaar.moonlight.api.resources.RPUtils;
-import net.mehvahdjukaar.moonlight.api.resources.pack.ResourceGenTask;
-import net.mehvahdjukaar.moonlight.api.resources.pack.ResourceSink;
 import net.mehvahdjukaar.moonlight.api.set.wood.VanillaWoodTypes;
 import net.mehvahdjukaar.moonlight.api.set.wood.WoodType;
 import net.mehvahdjukaar.moonlight.api.util.Utils;
 import net.minecraft.core.registries.Registries;
-import net.minecraft.resources.ResourceLocation;
-import net.minecraft.server.packs.resources.ResourceManager;
 import net.minecraft.tags.BlockTags;
 import net.minecraft.tags.ItemTags;
 import net.minecraft.world.item.CreativeModeTab;
@@ -39,11 +30,6 @@ import net.neoforged.neoforge.common.Tags;
 import net.neoforged.neoforge.registries.DeferredHolder;
 
 public class WoodModule extends EveryCompatModule {
-    // Moonlight auto-detects our own mushroot as a compat wood too; re-registering its WoodType here
-    // collides with WoodTypeRegistry.MUSHROOT and crashes HangingSignRenderer. Skip it - it already
-    // has full native support.
-    private static final String EXCLUDED_WOOD_TYPES = "superbb:mushroot";
-
     public final SimpleEntrySet<WoodType, Block> bridge;
     public final SimpleEntrySet<WoodType, Block> bridgeStairs;
     public final SimpleEntrySet<WoodType, Block> picketFence;
@@ -141,10 +127,12 @@ public class WoodModule extends EveryCompatModule {
                         BlockRegistry.OAK_WALL_ARROW_SIGN, () -> VanillaWoodTypes.OAK,
                         woodType -> new WallArrowSignBlock(woodType.toVanillaOrOak(), Utils.copyPropertySafe(woodType.planks)))
                 .addTile(BlockEntityRegistry.ARROW_SIGN)
-                .noItem()
+                .addTexture(modRes("item/arrow_sign/oak_arrow_sign"), PaletteStrategies.SIGN_LIKE)
+                .addModelTransform(transform -> transform.replaceItemType("oak"))
                 .addTag(BlockTags.MINEABLE_WITH_AXE, Registries.BLOCK)
-                .excludeBlockTypes(EXCLUDED_WOOD_TYPES)
+                .requiresChildren("planks")
                 .setTab(functionalBlocksTab)
+                .noItem()
                 .build();
         this.addEntry(wallArrowSign);
 
@@ -152,10 +140,12 @@ public class WoodModule extends EveryCompatModule {
                         BlockRegistry.OAK_HANGING_ARROW_SIGN, () -> VanillaWoodTypes.OAK,
                         woodType -> new HangingArrowSignBlock(woodType.toVanillaOrOak(), Utils.copyPropertySafe(woodType.planks)))
                 .addTile(BlockEntityRegistry.ARROW_SIGN)
-                .noItem()
+                .addTexture(modRes("item/arrow_sign/oak_arrow_sign"), PaletteStrategies.SIGN_LIKE)
+                .addModelTransform(transform -> transform.replaceItemType("oak"))
                 .addTag(BlockTags.MINEABLE_WITH_AXE, Registries.BLOCK)
-                .excludeBlockTypes(EXCLUDED_WOOD_TYPES)
+                .requiresChildren("planks")
                 .setTab(functionalBlocksTab)
+                .noItem()
                 .build();
         this.addEntry(hangingArrowSign);
 
@@ -166,11 +156,13 @@ public class WoodModule extends EveryCompatModule {
                 .addTextureM(modRes("entity/signs/arrow/oak_arrow_sign"), EveryCompat.res("block/mv/oak_arrow_sign_mask"),
                         PaletteStrategies.SIGN_LIKE)
                 .addTexture(modRes("item/arrow_sign/oak_arrow_sign"), PaletteStrategies.SIGN_LIKE)
+                .addModelTransform(transform -> transform.replaceItemType("oak"))
                 .addTag(BlockTags.MINEABLE_WITH_AXE, Registries.BLOCK)
-                .excludeBlockTypes(EXCLUDED_WOOD_TYPES)
                 .addCustomItem((woodType, block, properties) -> new ArrowSignItem(properties.stacksTo(16),
                         block, this.wallArrowSign.blocks.get(woodType), this.hangingArrowSign.blocks.get(woodType)))
+                .requiresChildren("planks")
                 .setTab(functionalBlocksTab)
+                .defaultRecipe()
                 .build();
         this.addEntry(arrowSign);
 
@@ -178,10 +170,12 @@ public class WoodModule extends EveryCompatModule {
                         BlockRegistry.LARGE_OAK_WALL_ARROW_SIGN, () -> VanillaWoodTypes.OAK,
                         woodType -> new LargeWallArrowSignBlock(woodType.toVanillaOrOak(), Utils.copyPropertySafe(woodType.planks)))
                 .addTile(BlockEntityRegistry.ARROW_SIGN)
-                .noItem()
+                .addTexture(modRes("item/large_arrow_sign/large_oak_arrow_sign"), PaletteStrategies.SIGN_LIKE)
+                .addModelTransform(transform -> transform.replaceItemType("oak"))
                 .addTag(BlockTags.MINEABLE_WITH_AXE, Registries.BLOCK)
-                .excludeBlockTypes(EXCLUDED_WOOD_TYPES)
+                .requiresChildren("planks")
                 .setTab(functionalBlocksTab)
+                .noItem()
                 .build();
         this.addEntry(largeWallArrowSign);
 
@@ -191,60 +185,14 @@ public class WoodModule extends EveryCompatModule {
                 .addTile(BlockEntityRegistry.ARROW_SIGN)
                 .addTexture(modRes("entity/signs/large_arrow/large_oak_arrow_sign"), PaletteStrategies.SIGN_LIKE)
                 .addTexture(modRes("item/large_arrow_sign/large_oak_arrow_sign"), PaletteStrategies.SIGN_LIKE)
+                .addModelTransform(transform -> transform.replaceItemType("oak"))
                 .addTag(BlockTags.MINEABLE_WITH_AXE, Registries.BLOCK)
-                .excludeBlockTypes(EXCLUDED_WOOD_TYPES)
                 .addCustomItem((woodType, block, properties) -> new LargeArrowSignItem(properties.stacksTo(16),
                         block, this.largeWallArrowSign.blocks.get(woodType)))
+                .requiresChildren("planks")
                 .setTab(functionalBlocksTab)
+                .defaultRecipe()
                 .build();
         this.addEntry(largeArrowSign);
-    }
-
-    private static net.minecraft.world.level.block.state.properties.WoodType vanillaWoodType(WoodType woodType) {
-        return net.minecraft.world.level.block.state.properties.WoodType.register(
-                new net.minecraft.world.level.block.state.properties.WoodType(
-                        Marioverse.MOD_ID + ":" + woodType.getId().getPath(), woodType.toVanillaOrOak().setType()));
-    }
-
-    // EveryCompat's automatic per-property blockstate generation fails silently for these blocks;
-    // write the same single-variant blockstate/model our own datagen uses instead.
-    @Override
-    public void addDynamicClientResources(Consumer<ResourceGenTask> executor) {
-        super.addDynamicClientResources(executor);
-        executor.accept((manager, sink) -> {
-            writeSimpleBlockModels(manager, sink, arrowSign);
-            writeSimpleBlockModels(manager, sink, wallArrowSign);
-            writeSimpleBlockModels(manager, sink, hangingArrowSign);
-            writeSimpleBlockModels(manager, sink, largeArrowSign);
-            writeSimpleBlockModels(manager, sink, largeWallArrowSign);
-        });
-    }
-
-    private static void writeSimpleBlockModels(ResourceManager manager, ResourceSink sink,
-                                               SimpleEntrySet<WoodType, Block> entrySet) {
-        for (Map.Entry<WoodType, Block> entry : entrySet.blocks.entrySet()) {
-            WoodType woodType = entry.getKey();
-            Block block = entry.getValue();
-            ResourceLocation blockId = Utils.getID(block);
-            ResourceLocation modelId = blockId.withPrefix("block/");
-
-            JsonObject variant = new JsonObject();
-            variant.addProperty("model", modelId.toString());
-            JsonObject variants = new JsonObject();
-            variants.add("", variant);
-            JsonObject blockstate = new JsonObject();
-            blockstate.add("variants", variants);
-            sink.addBlockState(blockId, blockstate);
-
-            JsonObject textures = new JsonObject();
-            try {
-                ResourceLocation particle = RPUtils.findFirstBlockTextureLocation(manager, woodType.planks);
-                textures.addProperty("particle", particle.toString());
-            } catch (Exception ignored) {
-            }
-            JsonObject model = new JsonObject();
-            model.add("textures", textures);
-            sink.addBlockModel(modelId, model);
-        }
     }
 }
