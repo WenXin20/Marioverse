07a926cfd0d42ba392c2fc17b0c4b8b89b3204b3
WenXin2 <dwnewville@gmail.com>
2026-09-06 16:41:43 -0500

    Middle click saves snowy state to item

--- src/main/java/com/wenxin2/marioverse/blocks/SnowyHedgeBlock.java
+++ src/main/java/com/wenxin2/marioverse/blocks/SnowyHedgeBlock.java
@@ -1,7 +1,15 @@
 package com.wenxin2.marioverse.blocks;
 
+import net.minecraft.core.BlockPos;
+import net.minecraft.core.component.DataComponents;
+import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.component.BlockItemStateProperties;
 import net.minecraft.world.item.context.BlockPlaceContext;
+import net.minecraft.world.level.LevelReader;
 import net.minecraft.world.level.block.state.BlockState;
+import net.minecraft.world.phys.HitResult;
+import org.jetbrains.annotations.NotNull;
 
 public class SnowyHedgeBlock extends HedgeBlock {
     public SnowyHedgeBlock(Properties properties) {
@@ -13,6 +21,16 @@ public class SnowyHedgeBlock extends HedgeBlock {
     @Override
     public BlockState getStateForPlacement(BlockPlaceContext context) {
         return super.getStateForPlacement(context).setValue(SNOWY, true);
+    }
+
+    @NotNull
+    @Override
+    public ItemStack getCloneItemStack(BlockState state, HitResult target, LevelReader level, BlockPos pos, Player player) {
+        ItemStack stack = super.getCloneItemStack(state, target, level, pos, player);
+
+        if (!state.getValue(SNOWY))
+            stack.set(DataComponents.BLOCK_STATE, BlockItemStateProperties.EMPTY.with(SNOWY, false));
+        return stack;
     }
 
     @Override
