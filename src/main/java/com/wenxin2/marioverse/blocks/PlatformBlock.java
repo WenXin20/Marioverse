40393718b7e37fe89d63226fd043c28dca6f837b
WenXin2 <dwnewville@gmail.com>
2026-09-19 18:00:12 -0500

    Improve log platform

--- src/main/java/com/wenxin2/marioverse/blocks/PlatformBlock.java
+++ src/main/java/com/wenxin2/marioverse/blocks/PlatformBlock.java
@@ -75,7 +75,7 @@ public class PlatformBlock extends SlabBlock {
             } else if (stateValue == SlabType.BOTTOM) {
                 if (!context.isAbove(TOP_COLLISION, pos.below(), false))
                     return Shapes.empty();
-            } else if (!context.isAbove(DOUBLE_COLLISION, pos, false))
+            } else if (!context.isAbove(TOP_COLLISION, pos, false) && !blockGetter.getBlockState(pos.above()).isSolid())
                 return Shapes.empty();
         }
         return super.getCollisionShape(state, blockGetter, pos, collisionContext);
